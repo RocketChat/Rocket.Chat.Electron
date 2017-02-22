e8813857446a423d281892450fd63cb0b4a932bb
Edipo Vinicius da Silva <edipox@gmail.com>
2017-02-22 14:35:01 -0300

    [macOS] Fix issues found by codacy bot

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -202,7 +202,30 @@ class SideBar extends EventEmitter {
         document.body.classList.remove('hide-server-list');
         localStorage.setItem('sidebar-closed', 'false');
         this.emit('show');
+<<<<<<< HEAD
         if (process.platform == 'darwin') {
+=======
+        this.addPlatformStyleForShow();
+    }
+
+    toggle () {
+        if (this.isHidden()) {
+            this.show();
+        } else {
+            this.hide();
+        }
+    }
+
+    addPlatformStyleForShow () {
+        if (process.platform === 'darwin') {
+            [].forEach.call(document.getElementsByTagName('webview'),
+                (webviewObj) => { if (webviewObj.insertCSS){ webviewObj.insertCSS('aside.side-nav{margin-top:0; overflow:hidden; transition: margin .5s ease-in-out; }'); }});
+        }
+    }
+
+    addPlatformStyleForHide () {
+        if (process.platform === 'darwin') {
+>>>>>>> [macOS] Fix issues found by codacy bot
             [].forEach.call(document.getElementsByTagName('webview'),
                 (webviewObj) => { if(webviewObj.insertCSS) { webviewObj.insertCSS('aside.side-nav{margin-top:0; overflow:hidden; transition: margin .5s ease-in-out; }'); } });
         }

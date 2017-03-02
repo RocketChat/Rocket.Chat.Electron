4762e17c8956f6c495f5f0b919d63e9e7c1ae808
pjakielaszek <pjakielaszek@bluesoft.net.pl>
2017-03-02 10:44:08 +0100

    Changed the function name and the switch is replaced with `icon-tray${title}.png`);

--- src/scripts/tray.js
+++ src/scripts/tray.js
@@ -24,17 +24,6 @@ const icons = {
 
 const _iconTray = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].icon || 'icon-tray.png');
 const _iconTrayAlert = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconAlert || 'icon-tray-alert.png');
-const _iconTrayTitleDot = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitleDot || 'title/icon-tray-title-dot.png');
-const _iconTrayTitle1 = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitle1 || 'title/icon-tray-title-1.png');
-const _iconTrayTitle2 = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitle2 || 'title/icon-tray-title-2.png');
-const _iconTrayTitle3 = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitle3 || 'title/icon-tray-title-3.png');
-const _iconTrayTitle4 = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitle4 || 'title/icon-tray-title-4.png');
-const _iconTrayTitle5 = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitle5 || 'title/icon-tray-title-5.png');
-const _iconTrayTitle6 = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitle6 || 'title/icon-tray-title-6.png');
-const _iconTrayTitle7 = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitle7 || 'title/icon-tray-title-7.png');
-const _iconTrayTitle8 = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitle8 || 'title/icon-tray-title-8.png');
-const _iconTrayTitle9 = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitle9 || 'title/icon-tray-title-9.png');
-const _iconTrayTitle9Plus = path.join(__dirname, 'images', icons[process.platform].dir, icons[process.platform].iconTrayTitle9Plus || 'title/icon-tray-title-9-plus.png');
 
 function createAppTray () {
     const _tray = new Tray(_iconTray);
@@ -98,44 +87,15 @@ function createAppTray () {
     };
 }
 
-function setTitle (title) {
-    switch (title) {
-        case "":
-            mainWindow.tray.setImage(_iconTray);
-            break;
-        case "•":
-            mainWindow.tray.setImage(_iconTrayTitleDot);
-            break;
-        case "1":
-            mainWindow.tray.setImage(_iconTrayTitle1);
-            break;
-        case "2":
-            mainWindow.tray.setImage(_iconTrayTitle2);
-            break;
-        case "3":
-            mainWindow.tray.setImage(_iconTrayTitle3);
-            break;
-        case "4":
-            mainWindow.tray.setImage(_iconTrayTitle4);
-            break;
-        case "5":
-            mainWindow.tray.setImage(_iconTrayTitle5);
-            break;
-        case "6":
-            mainWindow.tray.setImage(_iconTrayTitle6);
-            break;
-        case "7":
-            mainWindow.tray.setImage(_iconTrayTitle7);
-            break;
-        case "8":
-            mainWindow.tray.setImage(_iconTrayTitle8);
-            break;
-        case "9":
-            mainWindow.tray.setImage(_iconTrayTitle9);
-            break;
-        default:
-            mainWindow.tray.setImage(_iconTrayTitle9Plus);
+function setImage (title) {
+    if (title === '•') {
+        title = "Dot";
+    } else if (!isNaN(parseInt(title)) && title > 9) {
+        title = "9Plus";
     }
+
+    const _iconPath = path.join(__dirname, 'images', icons[process.platform].dir, `icon-tray${title}.png`);
+    mainWindow.tray.setImage(_iconPath);
 }
 
 function showTrayAlert (showAlert, title) {
@@ -145,7 +105,7 @@ function showTrayAlert (showAlert, title) {
 
     mainWindow.flashFrame(showAlert);
     if (process.platform !== 'darwin') {
-        setTitle(title);
+        setImage(title);
     } else {
         if (showAlert) {
             mainWindow.tray.setImage(_iconTrayAlert);

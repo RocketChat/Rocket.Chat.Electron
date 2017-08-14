9e42434414c700c1b358d5a178bac548fb68e5cc
Gabriel Delavald <gdelavald@gmail.com>
2017-08-14 17:34:29 -0300

    focus on window when clicking on notification (osx)

--- src/public/lib/Notification.js
+++ src/public/lib/Notification.js
@@ -6,6 +6,10 @@ if (process.platform === 'darwin') {
         constructor (title, options) {
             options.bundleId = `chat.rocket`;
             super(title, options);
+            this.addEventListener('click', (/*notification*/) => {
+                ipcRenderer.send('focus');
+                ipcRenderer.sendToHost('focus');
+            });
         }
 
         static requestPermission () {

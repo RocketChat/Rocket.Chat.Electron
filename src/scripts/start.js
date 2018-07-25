ad6118777d2e56b9a1e2a0d25f756c4766e35dfc
HappyTobi <tobiasschug@gmail.com>
2018-07-25 07:12:19 +0200

    [FIX] trailing slashes

--- src/scripts/start.js
+++ src/scripts/start.js
@@ -52,7 +52,6 @@ export const start = function () {
                 hostField.classList.remove('wrong');
 
                 let host = hostField.value.trim();
-                host = host.replace(/\/$/, '');
                 hostField.value = host;
 
                 if (host.length === 0) {

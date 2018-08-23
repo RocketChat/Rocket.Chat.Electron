8c4d8efd25da3e7209cd357be851af1134a4da98
Tasso Evangelista <tasso@tassoevan.me>
2018-08-23 00:26:34 -0300

    Use electron-builder API instead of cli and remove build config from package.json

--- tasks/release.js
+++ tasks/release.js
@@ -1,36 +1,15 @@
 'use strict';
 
 const gulp = require('gulp');
-const sequence = require('gulp-sequence');
-const childProcess = require('child_process');
-const os = require('os');
-const path = require('path');
+const { build } = require('electron-builder');
+const config = require('../electron-builder.json');
 const { getEnvName } = require('./utils');
 
-const argv = process.argv.slice(3).filter(arg => !arg.startsWith('--env'));
-const publishArgs = getEnvName() !== 'production' ? [ '--publish', 'never' ] : [];
+const publish = getEnvName() !== 'production' ? 'never' : null;
+gulp.task('release:darwin', () => build({ publish, x64: true, mac: [] }));
+gulp.task('release:win32', () => build({ publish, ia32: true, x64: true, win: [ 'nsis', 'appx' ] }));
+gulp.task('release:linux', () => build({ publish, x64: true, linux: [] })
+    .then(() => build({ publish, ia32: true, linux: config.linux.target.filter(target => target !== 'snap') }))
+);
 
-const buildRelease = (...args) => cb => {
-    const buildPath = path.join('node_modules', '.bin', os.platform() === 'win32' ? 'build.cmd' : 'build');
-    childProcess.spawn(buildPath, [ ...argv, ...publishArgs, ...args ], { stdio: 'inherit' })
-        .on('close', () => cb());
-};
-
-gulp.task('release:osx', [ 'build-app' ], buildRelease('--x64', '--mac'));
-gulp.task('release:win', [ 'build-app' ], buildRelease('--ia32', '--x64', '--win', 'nsis', 'appx'));
-gulp.task('release:linux-x64', buildRelease('--x64', '--linux'));
-gulp.task('release:linux-ia32', buildRelease('--ia32', '--linux', 'tar.gz', 'deb', 'rpm'));
-gulp.task('release:linux', [ 'build-app' ], sequence('release:linux-x64', 'release:linux-ia32'));
-
-gulp.task('release', cb => {
-    switch (os.platform()) {
-        case 'darwin':
-            return gulp.start('release:osx', cb);
-
-        case 'win32':
-            return gulp.start('release:win', cb);
-
-        case 'linux':
-            return gulp.start('release:linux', cb);
-    }
-});
+gulp.task('release', [ 'build-app', `release:${ process.platform }` ]);

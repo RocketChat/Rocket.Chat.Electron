e7bf7f9d8c222c51de095fa4f62e11d6bf490899
HappyTobi <tobiasschug@gmail.com>
2018-08-27 23:10:25 +0200

    [FIX] change import to es6 import

--- src/preferences.js
+++ src/preferences.js
@@ -1,5 +1,5 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from ('react');
+import ReactDOM from ('react-dom');
 
 class HelloMessage extends React.Component {
     render() {

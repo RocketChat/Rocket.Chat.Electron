df93f4159da402969e8a18d6d525e7b0c22ca795
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-08-29 23:38:35 -0300

    Revert "Remove Windows build for ia32 arch"
    This reverts commit ec504d074f02783b8e90ec149437e08f7d647bed.

--- tasks/release.js
+++ tasks/release.js
@@ -7,7 +7,7 @@ const { getEnvName } = require('./utils');
 
 const publish = getEnvName() !== 'production' ? 'never' : 'onTagOrDraft';
 gulp.task('release:darwin', () => build({ publish, x64: true, mac: [] }));
-gulp.task('release:win32', () => build({ publish, x64: true, win: [ 'nsis', 'appx' ] }));
+gulp.task('release:win32', () => build({ publish, ia32: true, x64: true, win: [ 'nsis', 'appx' ] }));
 gulp.task('release:linux', (cb) => {
     build({ publish, x64: true, linux: [] })
         .then(() => build({ publish, ia32: true, linux: config.linux.target.filter(target => target !== 'snap') }))

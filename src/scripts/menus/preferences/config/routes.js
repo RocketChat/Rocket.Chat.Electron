b225840794898a1bb61bed255eb0c137bd80d880
HappyTobi <tobiasschug@gmail.com>
2018-09-11 11:49:35 +0200

    [FIX] routes layout

--- src/scripts/menus/preferences/config/routes.js
+++ src/scripts/menus/preferences/config/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 
+import style from './routes.css'
+
 import Styles from '../views/styles';
-import Network from '../views/network';
+import Usability from '../views/usability';
 
 const routes = (
-    <div>
-      <Route path="/styles" component={Styles} />
-      <Route path="/network" component={Network} />      
+    <div className="rcr-routes-container">
+      <Route path="/" component={Styles} />
+      <Route path="/usability" component={Usability} />      
     </div>
   );
 

21db99e529915d0a1590ec1ec375747515087f6c
HappyTobi <tobiasschug@gmail.com>
2018-09-18 13:14:29 +0200

    [NEW] load jsonfile from user dir

--- src/scripts/menus/preferences/actions/loadAction.js
+++ src/scripts/menus/preferences/actions/loadAction.js
@@ -4,6 +4,13 @@ import { preferencesStoreFile, userDataDir } from '../config/statics'
 export default function () {
 
     const loadPreferences = function () {
+        let loadPreferences = {};
+        try {
+            loadPreferences = userDataDir.read(preferencesStoreFile, 'json');
+        } catch (err) {
+            //load default values...
+        }
+        return loadPreferences;
 
     }
 

d86ad1e6e124c06b7b242b40979490131f1c86e5
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-10-16 21:05:27 -0300

    Move code

--- src/app.js
+++ src/app.js
@@ -1,16 +1,5 @@
-import { shell } from 'electron';
 import { start } from './scripts/start';
 import './branding/branding.js';
 
 window.$ = window.jQuery = require('./vendor/jquery-3.1.1');
 start();
-
-window.addEventListener('load', () => {
-	document.addEventListener('click', (event) => {
-		const anchorElement = event.target.closest('a[rel="noopener noreferrer"]');
-		if (anchorElement) {
-			shell.openExternal(anchorElement.href);
-			event.preventDefault();
-		}
-	});
-});

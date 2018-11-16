3b00626a6490913468e77df9216d4f6d92f50d17
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-11-16 01:09:55 -0200

    Remove environment configuration files

--- tasks/build-app.js
+++ tasks/build-app.js
@@ -4,7 +4,6 @@ const gulp = require('gulp');
 const batch = require('gulp-batch');
 const less = require('gulp-less');
 const plumber = require('gulp-plumber');
-const rename = require('gulp-rename');
 const watch = require('gulp-watch');
 const bundle = require('./bundle');
 const utils = require('./utils');
@@ -29,10 +28,7 @@ gulp.task('less', () => gulp.src(srcDir.path('stylesheets/main.less'))
 	.pipe(less())
 	.pipe(gulp.dest(appDir.path('stylesheets'))));
 
-gulp.task('environment', () => gulp.src(configDir.path(`env_${ utils.getEnvName() }.json`))
-	.pipe(plumber())
-	.pipe(rename('env.json'))
-	.pipe(gulp.dest(appDir.path('.'))));
+gulp.task('environment', () => appDir.writeAsync('env.json', JSON.stringify({ name: utils.getEnvName() })));
 
 gulp.task('build-app', ['public', 'i18n', 'bundle', 'less', 'environment']);
 

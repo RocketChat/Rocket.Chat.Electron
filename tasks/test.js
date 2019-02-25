a4e40dcb08b98661c4a6368fe505803dd3778b88
Tasso Evangelista <tasso@tassoevan.me>
2019-02-25 14:21:46 -0300

    [CHORE] Rewrite Gulp tasks (#1105)
    * Remove unused npm script
    
    * Rewrite gulp tasks
    
    * Add stylelint
    
    * Run subprocesses inside a shell
    
    * Add e2e:run task for clarity

---
+++ tasks/test.js
@@ -0,0 +1,36 @@
+const childProcess = require('child_process');
+const gulp = require('gulp');
+const bundle = require('./bundle');
+const getEnv = require('./env');
+
+
+gulp.task('test:build', async() => {
+	const env = getEnv();
+	await bundle.many('src', 'background/*.spec.js', 'app/main.specs.js', { env });
+	await bundle.many('src', ['*.spec.js', '!background/*.spec.js'], 'app/renderer.specs.js', { env });
+});
+
+gulp.task('test:main', (cb) => {
+	childProcess
+		.spawn('xvfb-maybe', [
+			'electron-mocha',
+			'--require',
+			'source-map-support/register',
+			'app/main.specs.js',
+		], { stdio: 'inherit', shell: true })
+		.on('close', cb);
+});
+
+gulp.task('test:renderer', (cb) => {
+	childProcess
+		.spawn('xvfb-maybe', [
+			'electron-mocha',
+			'--require',
+			'source-map-support/register',
+			'--renderer',
+			'app/renderer.specs.js',
+		], { stdio: 'inherit', shell: true })
+		.on('close', cb);
+});
+
+gulp.task('test', gulp.series('env:test', gulp.parallel('build', 'test:build'), 'test:main', 'test:renderer'));

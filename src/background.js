af01e0c60af71ce85cda8ef905fad4583f28ff30
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-03-04 22:21:55 -0300

    Bypass single instance lock for MAS builds

--- src/background.js
+++ src/background.js
@@ -3,71 +3,90 @@ import querystring from 'querystring';
 import url from 'url';
 import appData from './background/appData';
 import certificate from './background/certificate';
-export { default as dock } from './background/dock';
 import { addServer, getMainWindow } from './background/mainWindow';
-export { default as menus } from './background/menus';
-export { default as notifications } from './background/notifications';
-export { default as remoteServers } from './background/servers';
 import './background/systemIdleTime';
-export { default as tray } from './background/tray';
 import './background/updates';
 import './background/dialogs/about';
 import './background/dialogs/screenshare';
 import './background/dialogs/update';
 import i18n from './i18n';
+export { default as dock } from './background/dock';
+export { default as menus } from './background/menus';
+export { default as tray } from './background/tray';
+export { default as notifications } from './background/notifications';
+export { default as remoteServers } from './background/servers';
 export { certificate };
 
 
-const parseProtocolUrls = (args) =>
-	args.filter((arg) => /^rocketchat:\/\/./.test(arg))
+function parseCommandLineArguments(args) {
+	args
+		.filter((arg) => /^rocketchat:\/\/./.test(arg))
 		.map((uri) => url.parse(uri))
 		.map(({ hostname, pathname, query }) => {
 			const { insecure } = querystring.parse(query);
 			return `${ insecure === 'true' ? 'http' : 'https' }://${ hostname }${ pathname || '' }`;
-		});
+		})
+		.slice(0, 1)
+		.forEach((serverUrl) => addServer(serverUrl));
+}
 
-const addServers = (protocolUrls) => parseProtocolUrls(protocolUrls)
-	.forEach((serverUrl) => addServer(serverUrl));
+function handleUncaughtException(error) {
+	console.error(error);
+	app.exit(1);
+}
 
-// macOS only
-app.on('open-url', (event, url) => {
-	event.preventDefault();
-	addServers([url]);
-});
+function handleUnhandledRejection(reason) {
+	console.error(reason);
+	app.exit(1);
+}
 
-app.on('window-all-closed', () => {
-	app.quit();
-});
+async function prepareApp() {
+	process.on('uncaughtException', handleUncaughtException);
+	process.on('unhandledRejection', handleUnhandledRejection);
 
-if (!app.isDefaultProtocolClient('rocketchat')) {
 	app.setAsDefaultProtocolClient('rocketchat');
-}
+	app.setAppUserModelId('chat.rocket');
 
-app.setAppUserModelId('chat.rocket');
-if (process.platform === 'linux') {
-	app.disableHardwareAcceleration();
-}
-app.commandLine.appendSwitch('--autoplay-policy', 'no-user-gesture-required');
+	// TODO: make it a setting
+	if (process.platform === 'linux') {
+		app.disableHardwareAcceleration();
+	}
 
-process.on('unhandledRejection', console.error.bind(console));
+	app.commandLine.appendSwitch('--autoplay-policy', 'no-user-gesture-required');
 
+	app.on('window-all-closed', () => {
+		app.quit();
+	});
 
-const gotTheLock = app.requestSingleInstanceLock();
+	app.on('open-url', (event, url) => {
+		event.preventDefault();
+		parseCommandLineArguments([url]);
+	});
 
-if (gotTheLock) {
 	app.on('second-instance', async(event, argv) => {
-		(await getMainWindow()).show();
-		addServers(argv.slice(2));
+		parseCommandLineArguments(argv.slice(2));
 	});
 
-	app.on('ready', async() => {
-		appData.initialize();
-		await i18n.initialize();
-		const mainWindow = await getMainWindow();
-		certificate.initWindow(mainWindow);
+	appData.initialize();
 
-		ipcMain.emit('check-for-updates');
-	});
-} else {
-	app.quit();
+	const canStart = process.mas || app.requestSingleInstanceLock();
+
+	if (!canStart) {
+		app.quit();
+		return;
+	}
 }
+
+async function startApp() {
+	await i18n.initialize();
+	const mainWindow = await getMainWindow();
+	parseCommandLineArguments(process.argv.slice(2));
+	certificate.initWindow(mainWindow);
+	ipcMain.emit('check-for-updates');
+}
+
+(async() => {
+	await prepareApp();
+	await app.whenReady();
+	await startApp();
+})();

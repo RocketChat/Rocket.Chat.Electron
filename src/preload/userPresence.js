a46f6310d9e7cf210d857d6d0f339f8ff065b812
Tasso Evangelista <tasso@tassoevan.me>
2019-04-15 22:39:10 -0700

    [FIX] Only set user presence as online when auto away detection is disabled (#1164)
    * Only set user presence as online when auto away detection is disabled
    
    * Disable user presence polling for users which disabled auto away

--- src/preload/userPresence.js
+++ src/preload/userPresence.js
@@ -2,64 +2,74 @@ import { ipcRenderer } from 'electron';
 import { getMeteor, getTracker, getGetUserPreference, getUserPresence } from './rocketChat';
 
 
-let maximumIdleTime = 10 * 1000;
-const idleDetectionInterval = 1 * 1000;
+const pollUserPresence = (UserPresence, maximumIdleTime) => {
+	let wasUserPresent = false;
 
-function onChangeUserPresence(isUserPresent) {
-	const UserPresence = getUserPresence();
+	return () => {
+		let isUserPresent = true;
 
-	if (!UserPresence) {
-		return;
-	}
+		try {
+			const idleTime = ipcRenderer.sendSync('request-system-idle-time');
+			isUserPresent = idleTime < maximumIdleTime;
 
-	if (isUserPresent) {
-		UserPresence.setOnline();
-		return;
-	}
+			if (isUserPresent === wasUserPresent) {
+				return;
+			}
 
-	UserPresence.setAway();
-}
+			if (isUserPresent) {
+				UserPresence.setOnline();
+			} else {
+				UserPresence.setAway();
+			}
+		} catch (error) {
+			console.error(error);
+		} finally {
+			wasUserPresent = isUserPresent;
+		}
+	};
+};
 
-let wasUserPresent = false;
-
-function pollUserPresence() {
-	let isUserPresent = false;
-
-	try {
-		const idleTime = ipcRenderer.sendSync('request-system-idle-time');
-		isUserPresent = idleTime < maximumIdleTime;
-	} catch (error) {
-		console.error(error);
-	}
-
-	if (isUserPresent !== wasUserPresent) {
-		onChangeUserPresence(isUserPresent);
-		wasUserPresent = isUserPresent;
-	}
-}
-
-function handleUserPresence() {
+const handleUserPresence = () => {
 	const Meteor = getMeteor();
 	const Tracker = getTracker();
 	const getUserPreference = getGetUserPreference();
+	const UserPresence = getUserPresence();
 
-	if (!Meteor || !Tracker || !getUserPreference) {
+	if (!Meteor || !Tracker || !getUserPreference || !UserPresence) {
 		return;
 	}
 
+	let intervalID;
+
 	Tracker.autorun(() => {
-		if (!Meteor.userId()) {
+		if (intervalID) {
+			clearInterval(intervalID);
+			intervalID = null;
+		}
+
+		const uid = Meteor.userId();
+
+		if (!uid) {
 			return;
 		}
 
-		const userId = Meteor.userId();
-		if (getUserPreference(userId, 'enableAutoAway')) {
-			maximumIdleTime = (getUserPreference(userId, 'idleTimeLimit') || 300) * 1000;
-		}
-	});
+		delete UserPresence.awayTime;
+		UserPresence.start();
 
-	setInterval(pollUserPresence, idleDetectionInterval);
-}
+		const isAutoAwayEnabled = getUserPreference(uid, 'enableAutoAway');
+
+		if (!isAutoAwayEnabled) {
+			UserPresence.setOnline();
+			return;
+		}
+
+		const maximumIdleTime = (getUserPreference(uid, 'idleTimeLimit') || 300) * 1000;
+		const idleTimeDetectionInterval = maximumIdleTime / 2;
+		const callback = pollUserPresence(UserPresence, maximumIdleTime);
+
+		intervalID = setInterval(callback, idleTimeDetectionInterval);
+	});
+};
 
 
 export default () => {

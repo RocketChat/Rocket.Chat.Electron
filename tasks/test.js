7155b405289f38ad329c0e3d56b2632208c49bb9
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-04-30 14:18:33 -0700

    [CHORE] Build configuration update (#1204)
    * Fix file indentation
    
    * Amend tasks to ESLint rules
    
    * Remove node_modules from electron-builder configuration
    
    * Remove 32-bit builds for Linux

--- tasks/test.js
+++ tasks/test.js
@@ -4,7 +4,7 @@ const bundle = require('./bundle');
 const getEnv = require('./env');
 
 
-gulp.task('test:build', async() => {
+gulp.task('test:build', async () => {
 	const env = getEnv();
 	await bundle.many('src', 'main/*.spec.js', 'app/main.specs.js', { env });
 	await bundle.many('src', ['*.spec.js', '!main/*.spec.js'], 'app/renderer.specs.js', { env });

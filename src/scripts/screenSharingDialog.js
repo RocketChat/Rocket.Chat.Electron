ebe513fdc7d132a4c9ce0051eda3644d5c02ac45
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-11-22 05:00:44 -0300

    Implement reactive UI in screen sharing dialog

--- src/scripts/screenSharingDialog.js
+++ src/scripts/screenSharingDialog.js
@@ -1,53 +1,63 @@
 import { desktopCapturer, ipcRenderer } from 'electron';
 import { t } from 'i18next';
 
+import { createElement, useRoot, useEffect, useRef } from './reactiveUi';
+
+function ScreenSharingDialog({ sources }) {
+	const sourceSelectedRef = useRef(false);
+
+	useEffect(() => {
+		document.title = t('dialog.screenshare.title');
+	});
+
+	useEffect(() => () => {
+		const { current: sourceSelected } = sourceSelectedRef;
+
+		if (sourceSelected) {
+			return;
+		}
+
+		ipcRenderer.send('select-screenshare-source', null);
+	}, []);
+
+	const handleScreenSharingSourceClick = (id) => () => {
+		sourceSelectedRef.current = true;
+		ipcRenderer.send('select-screenshare-source', id);
+		ipcRenderer.send('close-screenshare-dialog');
+	};
+
+	const root = useRoot();
+
+	root.querySelector('.screenshare-title').innerText = t('dialog.screenshare.announcement');
+
+	while (root.querySelector('.screenshare-sources').firstChild) {
+		root.querySelector('.screenshare-sources').firstChild.remove();
+	}
+
+	root.querySelector('.screenshare-sources').append(...sources.map(({ id, name, thumbnail }) => {
+		const sourceView = document.importNode(root.querySelector('.screenshare-source-template').content, true);
+
+		sourceView.querySelector('.screenshare-source').onclick = handleScreenSharingSourceClick(id);
+
+		sourceView.querySelector('.screenshare-source-thumbnail img').setAttribute('alt', name);
+		sourceView.querySelector('.screenshare-source-thumbnail img').setAttribute('src', thumbnail.toDataURL());
+
+		sourceView.querySelector('.screenshare-source-name').innerText = name;
+
+		return sourceView;
+	}));
+}
+
 const setupScreenSharingDialog = async () => {
 	const sources = await desktopCapturer.getSources({ types: ['window', 'screen'] });
 
-	let sourceSelected = false;
+	const element = createElement(ScreenSharingDialog, { sources });
 
-	const root = document.querySelector('.screenshare-page');
+	element.mount(document.querySelector('.screenshare-page'));
 
-	const render = () => {
-		// useEffect
-		document.title = t('dialog.screenshare.title');
-
-		// useEffect cleanUp deps=[]
-		window.onunload = () => {
-			if (sourceSelected) {
-				return;
-			}
-
-			ipcRenderer.send('select-screenshare-source', null);
-		};
-
-		const handleScreenSharingSourceClick = (id) => () => {
-			sourceSelected = true;
-			ipcRenderer.send('select-screenshare-source', id);
-			ipcRenderer.send('close-screenshare-dialog');
-		};
-
-		root.querySelector('.screenshare-title').innerText = t('dialog.screenshare.announcement');
-
-		while (root.querySelector('.screenshare-sources').firstChild) {
-			root.querySelector('.screenshare-sources').firstChild.remove();
-		}
-
-		root.querySelector('.screenshare-sources').append(...sources.map(({ id, name, thumbnail }) => {
-			const sourceView = document.importNode(root.querySelector('.screenshare-source-template').content, true);
-
-			sourceView.querySelector('.screenshare-source').onclick = handleScreenSharingSourceClick(id);
-
-			sourceView.querySelector('.screenshare-source-thumbnail img').setAttribute('alt', name);
-			sourceView.querySelector('.screenshare-source-thumbnail img').setAttribute('src', thumbnail.toDataURL());
-
-			sourceView.querySelector('.screenshare-source-name').innerText = name;
-
-			return sourceView;
-		}));
+	window.onunload = () => {
+		element.unmount();
 	};
-
-	render();
 };
 
 export default setupScreenSharingDialog;

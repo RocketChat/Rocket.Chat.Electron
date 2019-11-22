97dedaf3bbff21bf4abf7297766a4f5695531d97
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-11-22 09:52:21 -0300

    Implement reactive UI in update dialog

--- src/scripts/screenSharingDialog.js
+++ src/scripts/screenSharingDialog.js
@@ -46,6 +46,8 @@ function ScreenSharingDialog({ sources }) {
 
 		return sourceView;
 	}));
+
+	return null;
 }
 
 const setupScreenSharingDialog = async () => {
--- src/scripts/updateDialog.js
+++ src/scripts/updateDialog.js
@@ -3,82 +3,91 @@ import querystring from 'querystring';
 import { remote, ipcRenderer } from 'electron';
 import { t } from 'i18next';
 
+import { createElement, useRoot, useEffect, useRef } from './reactiveUi';
+
 const { app, dialog } = remote;
+
+function UpdateDialog({
+	currentVersion,
+	newVersion,
+}) {
+	useEffect(() => {
+		document.title = t('dialog.update.title');
+	});
+
+	const installButtonRef = useRef();
+
+	useEffect(() => {
+		installButtonRef.current.focus();
+	}, []);
+
+	const handleSkipButtonClick = async () => {
+		await dialog.showMessageBox(remote.getCurrentWindow(), {
+			type: 'warning',
+			title: t('dialog.updateSkip.title'),
+			message: t('dialog.updateSkip.message'),
+			buttons: [t('dialog.updateSkip.ok')],
+			defaultId: 0,
+		});
+		ipcRenderer.send('skip-update-version', newVersion);
+		ipcRenderer.send('close-update-dialog');
+	};
+
+	const handleRemindLaterButtonClick = () => {
+		ipcRenderer.send('remind-update-later');
+		ipcRenderer.send('close-update-dialog');
+	};
+
+	const handleInstallButtonClick = async () => {
+		await dialog.showMessageBox(remote.getCurrentWindow(), {
+			type: 'info',
+			title: t('dialog.updateDownloading.title'),
+			message: t('dialog.updateDownloading.message'),
+			buttons: [t('dialog.updateDownloading.ok')],
+			defaultId: 0,
+		});
+		ipcRenderer.send('download-update');
+		ipcRenderer.send('close-update-dialog');
+	};
+
+	const root = useRoot();
+
+	root.querySelector('.update-title').innerText = t('dialog.update.announcement');
+
+	root.querySelector('.update-message').innerText = t('dialog.update.message');
+
+	root.querySelector('.current-version .app-version-label').innerText = t('dialog.update.currentVersion');
+
+	root.querySelector('.current-version .app-version-value').innerText = currentVersion;
+
+	root.querySelector('.new-version .app-version-label').innerText = t('dialog.update.newVersion');
+
+	root.querySelector('.new-version .app-version-value').innerText = newVersion;
+
+	root.querySelector('.update-skip-action').innerText = t('dialog.update.skip');
+	root.querySelector('.update-skip-action').onclick = handleSkipButtonClick;
+
+	root.querySelector('.update-remind-action').innerText = t('dialog.update.remindLater');
+	root.querySelector('.update-remind-action').onclick = handleRemindLaterButtonClick;
+
+	installButtonRef.current = root.querySelector('.update-install-action');
+	root.querySelector('.update-install-action').innerText = t('dialog.update.install');
+	root.querySelector('.update-install-action').onclick = handleInstallButtonClick;
+
+	return null;
+}
 
 const setupUpdateDialog = () => {
 	const { newVersion } = querystring.parse(window.location.search.slice(1));
 	const currentVersion = app.getVersion();
 
-	let prevEmptyDeps;
-	let installButton;
+	const element = createElement(UpdateDialog, { currentVersion, newVersion });
 
-	const root = document.querySelector('.update-page');
+	element.mount(document.querySelector('.update-page'));
 
-	const render = () => {
-		// useEffect
-		document.title = t('dialog.update.title');
-
-		// useRef
-		installButton = root.querySelector('.update-install-action');
-
-		// useEffect deps=[]
-		if (!prevEmptyDeps) {
-			prevEmptyDeps = [];
-			installButton.focus();
-		}
-
-		const handleSkipButtonClick = async () => {
-			await dialog.showMessageBox(remote.getCurrentWindow(), {
-				type: 'warning',
-				title: t('dialog.updateSkip.title'),
-				message: t('dialog.updateSkip.message'),
-				buttons: [t('dialog.updateSkip.ok')],
-				defaultId: 0,
-			});
-			ipcRenderer.send('skip-update-version', newVersion);
-			ipcRenderer.send('close-update-dialog');
-		};
-
-		const handleRemindLaterButtonClick = () => {
-			ipcRenderer.send('remind-update-later');
-			ipcRenderer.send('close-update-dialog');
-		};
-
-		const handleInstallButtonClick = async () => {
-			await dialog.showMessageBox(remote.getCurrentWindow(), {
-				type: 'info',
-				title: t('dialog.updateDownloading.title'),
-				message: t('dialog.updateDownloading.message'),
-				buttons: [t('dialog.updateDownloading.ok')],
-				defaultId: 0,
-			});
-			ipcRenderer.send('download-update');
-			ipcRenderer.send('close-update-dialog');
-		};
-
-		root.querySelector('.update-title').innerText = t('dialog.update.announcement');
-
-		root.querySelector('.update-message').innerText = t('dialog.update.message');
-
-		root.querySelector('.current-version .app-version-label').innerText = t('dialog.update.currentVersion');
-
-		root.querySelector('.current-version .app-version-value').innerText = currentVersion;
-
-		root.querySelector('.new-version .app-version-label').innerText = t('dialog.update.newVersion');
-
-		root.querySelector('.new-version .app-version-value').innerText = newVersion;
-
-		root.querySelector('.update-skip-action').innerText = t('dialog.update.skip');
-		root.querySelector('.update-skip-action').onclick = handleSkipButtonClick;
-
-		root.querySelector('.update-remind-action').innerText = t('dialog.update.remindLater');
-		root.querySelector('.update-remind-action').onclick = handleRemindLaterButtonClick;
-
-		root.querySelector('.update-install-action').innerText = t('dialog.update.install');
-		root.querySelector('.update-install-action').onclick = handleInstallButtonClick;
+	window.onunload = () => {
+		element.unmount();
 	};
-
-	render();
 };
 
 export default setupUpdateDialog;

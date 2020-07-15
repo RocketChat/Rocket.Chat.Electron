f36c3b2cec286a608a93eb423c5bbd5df8e8dd0f
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-15 01:13:19 -0300

    Remove bundling for tests

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import builtinModules from 'builtin-modules';
-import glob from 'glob';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
@@ -11,60 +10,33 @@ import appManifest from './package.json';
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
-const bundleOptions = {
-	external: [
-		...builtinModules,
-		...Object.keys(appManifest.dependencies),
-		...Object.keys(appManifest.devDependencies),
-	],
-	plugins: [
-		copy({
-			targets: [
-				{ src: 'src/i18n/*.i18n.json', dest: 'app/i18n' },
-				{ src: 'src/public/*', dest: 'app/public' },
-				{ src: 'node_modules/@rocket.chat/icons/dist/*', dest: 'app/icons' },
-			],
-		}),
-		json(),
-		replace({
-			'process.env.BUGSNAG_API_KEY': JSON.stringify(process.env.BUGSNAG_API_KEY),
-			'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-		}),
-		babel({
-			babelHelpers: 'bundled',
-		}),
-		nodeResolve(),
-		commonjs(),
-	],
-};
-
-const createTestEntries = () => {
-	if (NODE_ENV !== 'test') {
-		return [];
-	}
-
-	const rendererSpecs = [
-		...glob.sync('src/**/*.spec.js'),
-	];
-	return [
-		...rendererSpecs.length
-			? [{
-				input: rendererSpecs,
-				...bundleOptions,
-				output: {
-					dir: 'app/renderer.specs',
-					format: 'cjs',
-					sourcemap: true,
-				},
-			}]
-			: [],
-	];
-};
-
 export default [
 	{
+		external: [
+			...builtinModules,
+			...Object.keys(appManifest.dependencies),
+			...Object.keys(appManifest.devDependencies),
+		],
 		input: 'src/main.js',
-		...bundleOptions,
+		plugins: [
+			copy({
+				targets: [
+					{ src: 'src/i18n/*.i18n.json', dest: 'app/i18n' },
+					{ src: 'src/public/*', dest: 'app/public' },
+					{ src: 'node_modules/@rocket.chat/icons/dist/*', dest: 'app/icons' },
+				],
+			}),
+			json(),
+			replace({
+				'process.env.BUGSNAG_API_KEY': JSON.stringify(process.env.BUGSNAG_API_KEY),
+				'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+			}),
+			babel({
+				babelHelpers: 'bundled',
+			}),
+			nodeResolve(),
+			commonjs(),
+		],
 		output: {
 			file: 'app/main.js',
 			format: 'cjs',
@@ -72,8 +44,24 @@ export default [
 		},
 	},
 	{
+		external: [
+			...builtinModules,
+			...Object.keys(appManifest.dependencies),
+			...Object.keys(appManifest.devDependencies),
+		],
 		input: 'src/app.js',
-		...bundleOptions,
+		plugins: [
+			json(),
+			replace({
+				'process.env.BUGSNAG_API_KEY': JSON.stringify(process.env.BUGSNAG_API_KEY),
+				'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+			}),
+			babel({
+				babelHelpers: 'bundled',
+			}),
+			nodeResolve(),
+			commonjs(),
+		],
 		output: [
 			{
 				file: 'app/app.js',
@@ -83,8 +71,24 @@ export default [
 		],
 	},
 	{
+		external: [
+			...builtinModules,
+			...Object.keys(appManifest.dependencies),
+			...Object.keys(appManifest.devDependencies),
+		],
 		input: 'src/preload.js',
-		...bundleOptions,
+		plugins: [
+			json(),
+			replace({
+				'process.env.BUGSNAG_API_KEY': JSON.stringify(process.env.BUGSNAG_API_KEY),
+				'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+			}),
+			babel({
+				babelHelpers: 'bundled',
+			}),
+			nodeResolve(),
+			commonjs(),
+		],
 		output: [
 			{
 				file: 'app/preload.js',
@@ -93,5 +97,4 @@ export default [
 			},
 		],
 	},
-	...createTestEntries(),
 ];

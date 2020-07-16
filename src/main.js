5dccfe8852e4d0988023aa7a3210590ad7b7f565
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-15 23:38:32 -0300

    Add main/startup module

--- src/main.js
+++ src/main.js
@@ -1,53 +1,18 @@
 import { app, BrowserWindow } from 'electron';
-import rimraf from 'rimraf';
 
 import { setupDevelopmentTools } from './main/dev';
 import { setupErrorHandling } from './main/errors';
+import { handleStartup } from './main/startup';
 
 if (require.main === module) {
 	setupDevelopmentTools();
 	setupErrorHandling();
+	handleStartup();
 }
 
 const preventEvent = (event) => event.preventDefault();
 
 const prepareApp = () => {
-	app.setAsDefaultProtocolClient('rocketchat');
-	app.setAppUserModelId('chat.rocket');
-
-	const [command, args] = [
-		process.argv.slice(0, app.isPackaged ? 1 : 2),
-		process.argv.slice(app.isPackaged ? 1 : 2),
-	];
-
-	if (args.includes('--disable-gpu')) {
-		app.commandLine.appendSwitch('--disable-2d-canvas-image-chromium');
-		app.commandLine.appendSwitch('--disable-accelerated-2d-canvas');
-		app.commandLine.appendSwitch('--disable-gpu');
-	}
-
-	if (args.includes('--reset-app-data')) {
-		const dataDir = app.getPath('userData');
-		rimraf.sync(dataDir);
-		app.relaunch({ args: [...command.slice(1)] });
-		app.exit();
-		return;
-	}
-
-	const canStart = process.mas || app.requestSingleInstanceLock();
-
-	if (!canStart) {
-		app.exit();
-		return;
-	}
-
-	app.commandLine.appendSwitch('--autoplay-policy', 'no-user-gesture-required');
-
-	// TODO: make it a setting
-	if (process.platform === 'linux') {
-		app.disableHardwareAcceleration();
-	}
-
 	app.addListener('certificate-error', preventEvent);
 	app.addListener('select-client-certificate', preventEvent);
 	app.addListener('login', preventEvent);

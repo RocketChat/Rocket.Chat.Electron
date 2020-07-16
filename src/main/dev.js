ba2b19da89f176cbe9ec1693ca9975ea768d7a1e
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-16 02:18:18 -0300

    Replace electron-reload with electron-reloader

--- src/main/dev.js
+++ src/main/dev.js
@@ -2,16 +2,14 @@ import path from 'path';
 
 import { app } from 'electron';
 // import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
-import setupElectronReload from 'electron-reload';
+import setupElectronReloader from 'electron-reloader';
 
 export const setupDevelopmentTools = () => {
 	if (process.env.NODE_ENV !== 'development') {
 		return;
 	}
 
-	setupElectronReload(path.join(app.getAppPath(), 'app'), {
-		electron: process.execPath,
-	});
+	setupElectronReloader(require.main);
 
 	// Waiting for https://github.com/MarshallOfSound/electron-devtools-installer/pull/140 to be merged
 	// app.whenReady().then(() => installExtension(REACT_DEVELOPER_TOOLS));

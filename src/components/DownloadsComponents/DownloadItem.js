26d18109c7ea97873cb14bee42352c02ac445c75
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-07-31 12:51:03 -0300

    Review

--- src/components/DownloadsComponents/DownloadItem.js
+++ src/components/DownloadsComponents/DownloadItem.js
@@ -1,52 +1,59 @@
-import { Box, Margins, Tile, Grid, Icon, Button } from '@rocket.chat/fuselage';
-import React, { useEffect, useState, useMemo } from 'react';
+import { Box, Grid, Icon, Button, ButtonGroup } from '@rocket.chat/fuselage';
+import { useMutableCallback, useDebouncedState } from '@rocket.chat/fuselage-hooks';
+import React, { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { ipcRenderer, remote, clipboard } from 'electron';
-import { Progress } from 'react-sweet-progress';
+import { Progress as SweetProgress } from 'react-sweet-progress';
 
 import 'react-sweet-progress/lib/style.css';
 import { formatBytes } from '../../downloadUtils';
 
 
+const STATUS = {
+	CANCELLED: 'cancelled',
+	PAUSED: 'paused',
+};
+
 // Recieve props for individual download item
-export default function DownloadItem(props) {
+export default function DownloadItem({
+	url,
+	fileName,
+	totalBytes,
+	itemId,
+	mime,
+	updateDownloads,
+	date = new Date(itemId).toDateString(),
+	Mbps: mbps,
+	serverTitle,
+	fileSize = formatBytes(totalBytes, 2, true),
+	...props
+}) {
 	// console.log(props);
 	const servers = useSelector(({ servers }) => servers);
-	// console.log(servers);
-	let paused = false;
-	const { url } = props;
-	const { fileName } = props;
-	const { totalBytes } = props;
-	const { itemId } = props;
-	const { mime } = props;
-	const { updateDownloads } = props;
-	const date = props.date || new Date(itemId).toDateString();
-	const fileSize = props.fileSize || formatBytes(props.totalBytes, 2, true);
-	const [percentage, setPercentage] = useState(props.percentage || 0);
-	const [path, setPath] = useState(props.path || '');
-	const [status, setStatus] = useState(props.status || 'All Downloads');
 
-	const completed = useMemo(() => percentage === 100, [percentage]);
-	let serverTitle;
-	const Mbps = props.Mbps || 0;
+	const [percentage, setPercentage] = useDebouncedState(props.percentage || 0, 100);
+	const [path, setPath] = useDebouncedState(props.path || '', 100);
+	const [status, setStatus] = useDebouncedState(props.status || 'All Downloads', 100);
 
-	if (props.serverTitle) {
-		serverTitle = props.serverTitle;
-	} else {
+	const completed = percentage === 100;
+	const paused = status === STATUS.PAUSED;
+
+
+	if (!serverTitle) {
 		const index = servers.findIndex(({ webContentId }) => webContentId === props.serverId);
 		serverTitle = servers[index].title;
 	}
 
 
-	const handleProgress = (event, data) => {
+	const handleProgress = useMutableCallback((event, data) => {
 		console.log('Progress');
 		// console.log(` Current Bytes: ${ bytes }`);
 		const percentage = Math.floor((data.bytes / totalBytes) * 100);
 		updateDownloads({ status: 'All Downloads', percentage, serverTitle, itemId, Mbps: data.Mbps });
 		setPercentage(percentage);
 		setPath(data.savePath);
-		console.log(data.Mbps);
-	};
+	});
+
 	// TODO: Convert to only recieve dynamic progressed bytes data. NEED TO THROTTLE
 	useEffect(() => {
 		// Listen on unique event only
@@ -54,7 +61,7 @@ export default function DownloadItem(props) {
 		return () => {
 			ipcRenderer.removeListener(`downloading-${ itemId }`, handleProgress);
 		};
-	});
+	}, [handleProgress, itemId]);
 
 
 	// Download Completed, Send data back
@@ -70,113 +77,171 @@ export default function DownloadItem(props) {
 		return () => {
 			ipcRenderer.removeListener(`download-complete-${ itemId }`, downloadComplete);
 		};
+	}, [date, fileName, fileSize, itemId, mime, props, serverTitle, setStatus, url]);
+
+	const handleCancel = useMutableCallback(() => {
+		setStatus(STATUS.CANCELLED);
+		ipcRenderer.send(`cancel-${ itemId }`);
+		props.updateDownloads({ status: STATUS.CANCELLED, percentage, itemId });
+		ipcRenderer.send('download-complete', { status: STATUS.CANCELLED, url, fileName, fileSize, percentage, serverTitle, itemId, date, path, mime });
 	});
 
-
-	const handleCancel = () => {
-		setStatus('Cancelled');
-		ipcRenderer.send(`cancel-${ itemId }`);
-		props.updateDownloads({ status: 'Cancelled', percentage, itemId });
-		ipcRenderer.send('download-complete', { status: 'Cancelled', url, fileName, fileSize, percentage, serverTitle, itemId, date, path, mime });
-	};
-	const handlePause = () => {
+	const handlePause = useMutableCallback(() => {
 		console.log(percentage);
-		setStatus('Paused');
+		setStatus(STATUS.PAUSED);
 		ipcRenderer.send(`pause-${ itemId }`);
-		props.updateDownloads({ status: 'Paused', percentage, itemId });
-		paused = !paused;
-	};
-	const handleRetry = () => {
+		props.updateDownloads({ status: STATUS.PAUSED, percentage, itemId });
+	});
+
+	const handleRetry = useMutableCallback(() => {
 		remote.getCurrentWebContents().downloadURL(`${ url }#${ serverTitle }`);
 		props.clear(itemId);
-	};
+	});
 
-	// const printProps = () => {
-	// 	console.log({ path, totalBytes, percentage, completed, status, serverTitle, mime });
-	// };
+	const handleDelete = useMutableCallback(() => props.clear(itemId));
 
-	return props.layout === 'compact' ? (<Box width='100%' display='flex' justifyContent='center'>
-		<Grid.Item sm={ 2 }>
-			<Box display='flex' justifyContent='space-between' alignItems='center' height='50px'>
-				{/* <img src={ image } height='30px' width='30px' style={ { borderRadius: '5px' } } alt="" /> */ }
-				<Box fontScale='p2'>{ fileName }</Box>
-				{/* Completed */ }
-				<Box is={ Button } ghost display={ completed ? 'inline' : 'none' } onClick={ () => props.handleFileOpen(path) } style={ { textDecoration: 'none', color: '#2F80ED' } }><Icon name='chevron-up'></Icon></Box>
-				{/* Progressing and Paused */ }
-				<Box is={ Button } ghost display={ completed || status === 'Cancelled' ? 'none' : 'inline' } onClick={ () => handlePause() } style={ { textDecoration: 'none', color: '#2F80ED' } }>{ paused ? <Icon name='play'></Icon> : <Icon name='pause'></Icon> }</Box>
-				<Box is={ Button } ghost display={ completed || status === 'Cancelled' ? 'none' : 'inline' } onClick={ () => handleCancel() } style={ { textDecoration: 'none', color: '#2F80ED' } }><Icon name='cross'></Icon></Box>
-				{/* Cancelled */ }
-				<Box is={ Button } ghost display={ status === 'Cancelled' ? 'inline' : 'none' } onClick={ handleRetry } style={ { textDecoration: 'none', color: '#2F80ED' } }><Icon name='refresh'></Icon></Box>
-			</Box>
-		</Grid.Item>
+	const handleFileOpen = useMutableCallback(() => props.handleFileOpen(path));
 
-		<Grid.Item sm={ 1 }>
-			<Box fontWeight='p2' height='50px' verticalAlign='middle'>
-				<p>{ serverTitle }</p>
-			</Box>
-		</Grid.Item>
-		<Grid.Item sm={ 1 }>
-			<Box fontWeight='p2' height='50px' verticalAlign='middle'>
-				<p>{ fileSize }</p>
-			</Box>
-		</Grid.Item>
-		<Grid.Item sm={ 1 }>
-			<Box fontWeight='p2' height='50px' verticalAlign='middle'>
-				<p>{ Mbps ? `${ Mbps }Mbps/s` : null }</p>
-			</Box>
-		</Grid.Item>
-		<Grid.Item sm={ 1 }>
-			<Box display='flex' alignItems='center' height='50px' >
-				<Progress theme={ { default: { color: '#2F80ED' } } } percent={ percentage } status='default' />
-			</Box>
-		</Grid.Item>
-		<Grid.Item sm={ 1 }>
-			<Box fontWeight='p2' height='50px' verticalAlign='middle'>
-				<p>{ date }</p>
-			</Box>
-		</Grid.Item>
-		<Grid.Item sm={ 1 }>
-			<Box display='flex' alignItems='center' height='50px'>
-				<Icon name='kebab'></Icon>
-			</Box>
-		</Grid.Item>
-	</Box>) : (<Margins all='x32'>
-		{/* <Grid md={true}> */ }
-		<Tile elevation='2' style={ { width: '95%' } }>
-			<Box height='11.5rem' width='100%' display='flex' alignItems='center'>
-				<Grid.Item xl={ 2 } sm={ 2 } style={ { display: 'flex', alignItems: 'center', justifyContent: 'center' } }>
-					<Box height='150px' width='150px' backgroundColor='lightgrey' borderRadius='10px' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
-						<Icon size='7rem' name='clip' />
-						<Box fonScale='s2' color='primary-500' display='block'>{ mime.split('/')[1] }</Box>
-					</Box>
-				</Grid.Item>
-				<Grid.Item xl={ 9 } sm={ 5 } style={ { height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'space-around', padding: '1.2rem 0' } }>
-					<Box fontSize='h1' lineHeight='h1'>{ fileName }</Box>
-					<Box display='flex' flexDirection='row' justifyContent='space-between' width='100%'>
-						<Box fontSize='s2' color='info'>{ serverTitle }</Box> <Box fontSize='s2' color='info'> { date }</Box> <Box fontSize='s2' color='info'>{ fileSize || '25MB' }</Box>
-						<Box fontSize='s2' color='info'>{ Mbps ? `${ Mbps }Mbps/s` : null }</Box>
-						{/* ESTIMATED (TODO) */ }
-						{/* <Box fontSize='s2' color='info'>{ '60s Left' }</Box> */ }
-					</Box>
-					<Progress theme={ { default: { color: '#2F80ED' } } } percent={ percentage } status='default' />
-					{/* // TODO: Implement Show in Folder */ }
-					<Box display='flex' flexDirection='row' justifyContent='space-between'>
-						{/* Completed */ }
-						<Box is={ Button } ghost display={ completed ? 'inline' : 'none' } onClick={ () => props.handleFileOpen(path) } style={ { textDecoration: 'none', color: '#2F80ED' } }>Show in Folder</Box>
-						<Box is={ Button } ghost display={ completed ? 'inline' : 'none' } onClick={ () => clipboard.write({ text: url }) } style={ { textDecoration: 'none', color: '#2F80ED' } }>Copy Link</Box>
-						{/* Progressing and Paused */ }
-						<Box is={ Button } ghost display={ completed || status === 'Cancelled' ? 'none' : 'inline' } onClick={ () => handlePause() } style={ { textDecoration: 'none', color: '#2F80ED' } }>{ paused ? 'Resume' : 'Pause' }</Box>
-						<Box is={ Button } ghost display={ completed || status === 'Cancelled' ? 'none' : 'inline' } onClick={ () => handleCancel() } style={ { textDecoration: 'none', color: '#2F80ED' } }>Cancel</Box>
-						{/* Cancelled */ }
-						<Box is={ Button } ghost display={ status === 'Cancelled' ? 'inline' : 'none' } onClick={ handleRetry } style={ { textDecoration: 'none', color: '#2F80ED' } }>Retry</Box>
+	// TODO TOAST
+	const handleCopyLink = useMutableCallback(() => clipboard.write({ text: url }));
 
-					</Box>
-				</Grid.Item>
-				<Grid.Item xl={ 1 } sm={ 1 } style={ { display: 'flex', justifyContent: 'center' } }>
-					<Button ghost onClick={ () => props.clear(itemId) } ><Icon name='cross' size='x32' /></Button>
-				</Grid.Item>
-			</Box>
-		</Tile>
-		{/* </Grid> */ }
-	</Margins>);
+	return props.layout === 'compact' ? <Compact
+		serverTitle={serverTitle}
+		mime={ mime.split('/')[1] }
+		date={date}
+		fileName={fileName}
+		fileSize={fileSize}
+		mbps={mbps}
+		percentage={percentage}
+		isCompleted={completed}
+		isPaused={paused}
+		isCancelled={status !== STATUS.CANCELLED}
+		handleFileOpen={handleFileOpen}
+		handleCopyLink={handleCopyLink}
+		handlePause={handlePause}
+		handleCancel={handleCancel}
+		handleRetry={handleRetry}
+		handleDelete={handleDelete}
+		mb = {props.mb}/> : <Extended
+		serverTitle={serverTitle}
+		mime={ mime.split('/')[1] }
+		date={date}
+		fileName={fileName}
+		fileSize={fileSize}
+		mbps={mbps}
+		percentage={percentage}
+		isCompleted={completed}
+		isPaused={paused}
+		isCancelled={status !== STATUS.CANCELLED}
+		handleFileOpen={handleFileOpen}
+		handleCopyLink={handleCopyLink}
+		handlePause={handlePause}
+		handleCancel={handleCancel}
+		handleRetry={handleRetry}
+		handleDelete={handleDelete}
+		mb = {props.mb}
+	/>;
 }
+
+const Compact = React.memo(({
+	serverTitle,
+	mime,
+	date,
+	fileName,
+	fileSize,
+	mbps,
+	percentage,
+	isCompleted,
+	isPaused,
+	isCancelled,
+	handleFileOpen,
+	handleCopyLink,
+	handlePause,
+	handleCancel,
+	handleRetry,
+	handleDelete,
+	...props
+}) => <Box width='100%' display='flex' justifyContent='center' flexDirection='column' alignItems='center' {...props}>
+	<Box display='flex' flexGrow={1} width='100%'>
+		<Box fontScale='p1' withTruncatedText flexGrow={1}>{ fileName }</Box>
+		<Box display='flex' justifyContent='space-between' alignItems='center'>
+			{/* <img src={ image } height='30px' width='30px' style={ { borderRadius: '5px' } } alt="" /> */ }
+			{/* TODO INSERT TITLES FOR EACH ACTION */}
+			{/* Completed */ }
+			{ isCompleted && <ActionButton onClick={ handleFileOpen }><Icon name='chevron-up'/></ActionButton>}
+			{/* Progressing and Paused */ }
+			{ !isCompleted && !isCancelled && <ActionButton onClick={ handlePause }>{ isPaused ? <Icon name='play'/> : <Icon name='pause'/> }</ActionButton> }
+			{ !isCompleted && !isCancelled && <ActionButton onClick={ handleCancel }><Icon name='cross'/></ActionButton> }
+			{/* Cancelled */ }
+			{ isCancelled && <ActionButton onClick={ handleRetry }><Icon name='refresh'/></ActionButton>}
+		</Box>
+	</Box>
+	<Box display='flex' flexGrow={1} width='100%'>
+		<Info withTruncatedText>{ serverTitle }</Info>
+		<Info withTruncatedText mi='x8'>{ fileSize }</Info>
+		{ mbps && <Info mi='x8'>{`${ mbps }Mbps/s`}</Info> }
+		<Progress percent={ percentage } mi='x8'/>
+		<Info withTruncatedText mi='x8'>{ date }</Info>
+		{/* <Box display='flex' alignItems='center'>
+			<Icon name='kebab'/> */}
+		{/* </Box> */}
+	</Box>
+</Box>);
+
+const Extended = React.memo(({
+	serverTitle,
+	mime,
+	date,
+	fileName,
+	fileSize,
+	mbps,
+	percentage,
+	isCompleted,
+	isPaused,
+	isCancelled,
+	handleFileOpen,
+	handleCopyLink,
+	handlePause,
+	handleCancel,
+	handleRetry,
+	handleDelete,
+	...props
+}) => <Box width='100%' display='flex' alignItems='center' {...props}>
+	{/* USE AVATAR FUSELAGE (TODO) */ }
+	<Box size='x124' flexShrink={0} bg='neutral-500-50' borderRadius='4px' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
+		<Icon size='x60' name='clip' />
+		<Box fonScale='s2' color='primary-500' display='block'>{ mime }</Box>
+	</Box>
+	<Box display='flex' flexDirection='column' flexGrow={1} mi='x16'>
+		<Box fontSize='s2' withTruncatedText color='default' pbe='x8'>{ fileName }</Box>
+		<Box display='flex' flexDirection='row' justifyContent='space-between' mb='x8'>
+			<Info>{ serverTitle }</Info>
+			<Info> { date }</Info>
+			<Info>{ fileSize || '25MB' }</Info>
+			{ mbps > 0 && <Info>{ `${ mbps }Mbps/s` }</Info>}
+			{/* ESTIMATED (TODO) */ }
+			{/* <Box fontSize='s2' color='info'>{ '60s Left' }</Box> */ }
+		</Box>
+		<Box mb='x8'>
+			<Progress percent={ percentage } />
+		</Box>
+		{/* // TODO: Implement Show in Folder */ }
+		<ButtonGroup>
+			{/* Completed */ }
+			{ isCompleted && <ActionButton onClick={ handleFileOpen }>Show in Folder</ActionButton> }
+			{ isCompleted && <ActionButton onClick={ handleCopyLink }>Copy Link</ActionButton> }
+			{/* Progressing and Paused */ }
+			{ !isCompleted && !isCancelled && <ActionButton onClick={ handlePause }>{ isPaused ? 'Resume' : 'Pause' }</ActionButton>}
+			{ !isCompleted && !isCancelled && <ActionButton onClick={ handleCancel }>Cancel</ActionButton>}
+			{/* Cancelled */ }
+			{ isCancelled && <ActionButton onClick={ handleRetry }>Retry</ActionButton>}
+			<ActionButton onClick={ handleDelete }>Delete</ActionButton>
+		</ButtonGroup>
+	</Box>
+</Box>);
+
+const Info = (props) => <Box fontSize='s1' color='info' {...props}/>;
+
+const ActionButton = (props) => <Button color='primary-500' small ghost {...props}/>;
+
+const Progress = ({ percent, ...props }) => <Box flexGrow={1} {...props}><SweetProgress theme={ useMemo(() => ({ default: { color: '#2F80ED' } }), []) } percent={ percent } status='default' /> </Box>;
--- src/components/DownloadsManagerView/index.js
+++ src/components/DownloadsManagerView/index.js
@@ -1,10 +1,11 @@
 import { Box, Grid, SearchInput, Select, Icon, Button, Tabs, Tooltip } from '@rocket.chat/fuselage';
+import { useLocalStorage } from '@rocket.chat/fuselage-hooks';
 // import { useTranslation } from 'react-i18next';
 import React, { useState, useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { ipcRenderer, shell } from 'electron';
 
-import { Wrapper, Content } from './styles';
+import { Wrapper } from './styles';
 import DownloadItem from '../DownloadsComponents/DownloadItem';
 import { mapping } from '../../downloadUtils';
 
@@ -17,11 +18,12 @@ export function DownloadsManagerView() {
 
 	// Downloads Array
 	const [downloads, setDownloads] = useState([]);
-	const [tab, setTab] = useState('All Downloads');
+
+	const [tab, setTab] = useLocalStorage('download-tab', 'All Downloads');
 	const [searchVal, setSearchVal] = useState('');
-	const [serverVal, setServerVal] = useState('');
-	const [typeVal, setTypeVal] = useState('');
-	const [layout, setLayout] = useState('expanded');
+	const [serverVal, setServerVal] = useLocalStorage('download-server', '');
+	const [typeVal, setTypeVal] = useLocalStorage('download-type', '');
+	const [layout, setLayout] = useLocalStorage('download-layout', 'expanded');
 	let timeHeading;
 
 	const handleFileOpen = (path) => {
@@ -35,6 +37,7 @@ export function DownloadsManagerView() {
 			setTab(event.target.innerText);
 		}
 	};
+
 	const handleLayout = () => {
 		if (layout === 'compact') {
 			setLayout('expanded');
@@ -129,107 +132,105 @@ export function DownloadsManagerView() {
 
 
 	return <Wrapper isVisible={ isVisible }>
-		<Content>
-			<Box width='85%'>
+		<Box p='x24'>
 
-				<Grid xl={ true } >
+			<Grid xl={ true } >
+				<Grid.Item xl={ 10 }>
+					<Box>
+						<Box fontSize='x32' lineHeight='2'>Downloads</Box>
+						<Box fontSize='x20' lineHeight='2' color='info'>See all your downloads here</Box>
+					</Box>
+				</Grid.Item>
 
-					<Grid.Item xl={ 12 } style={ { display: 'flex', justifyContent: 'space-between', alignItems: 'center' } }>
-						<Grid.Item xl={ 3 } sm={ 2 } >
-							<SearchInput width='150px' onChange={ handleSearch } placeholder='Search' addon={ <Icon name='send' size='x20' /> } />
-						</Grid.Item>
+				<Grid.Item xl={ 12 }>
+					<Tabs>
+						<Tabs.Item selected={ tab === 'All Downloads' } onClick={ handleTabChange }>All Downloads</Tabs.Item>
+						<Tabs.Item selected={ tab === 'Paused' } onClick={ handleTabChange }>Paused</Tabs.Item>
+						<Tabs.Item selected={ tab === 'Cancelled' } onClick={ handleTabChange }>Cancelled</Tabs.Item>
+					</Tabs>
+				</Grid.Item>
 
-						<Grid.Item xl={ 3 } sm={ 2 } >
-							<Select width='100%' onChange={ handleServerFilter } placeholder='Filter by Server' options={ options } />
+				<Grid.Item xl={ 12 } style={ { display: 'flex', justifyContent: 'space-between', alignItems: 'center' } }>
+					<Grid.Item xl={ 3 } sm={ 2 } >
+						<SearchInput width='150px' onChange={ handleSearch } placeholder='Search' addon={ <Icon name='send' size='x20' /> } />
+					</Grid.Item>
 
-						</Grid.Item>
+					<Grid.Item xl={ 3 } sm={ 2 } >
+						<Select width='100%' onChange={ handleServerFilter } placeholder='Filter by Server' options={ options } />
 
-						<Grid.Item xl={ 2 } sm={ 2 } >
-							<Select width='100%' onChange={ handleMimeFilter } placeholder='Filter by File type' options={ fileTypes } />
-						</Grid.Item>
+					</Grid.Item>
 
-						<Grid.Item xl={ 1 } sm={ 1 } >
-							<Box width='100%' textAlign='end'>
-								<Button ghost onClick={ handleLayout }>
-									<Icon name='medium-view' size='x32' />
-									<Box>Change View</Box>
-								</Button>
-							</Box>
+					<Grid.Item xl={ 2 } sm={ 2 } >
+						<Select width='100%' onChange={ handleMimeFilter } placeholder='Filter by File type' options={ fileTypes } />
+					</Grid.Item>
 
-						</Grid.Item>
-
-						<Grid.Item xl={ 1 } sm={ 1 } className='tooltip' >
-							<Button ghost onClick={ clearAll }>
-								<Icon name='trash' size='x32' />
-								<Box>Delete All</Box>
+					<Grid.Item xl={ 1 } sm={ 1 } >
+						<Box width='100%' textAlign='end'>
+							<Button ghost onClick={ handleLayout }>
+								<Icon name='medium-view' size='x32' />
+								<Box>Change View</Box>
 							</Button>
-						</Grid.Item>
-					</Grid.Item>
-					<Grid.Item xl={ 10 }>
-						<Box>
-							<Box fontSize='x32' lineHeight='2'>Downloads</Box>
-							<Box fontSize='x20' lineHeight='2' color='info'>See all your downloads here</Box>
 						</Box>
-					</Grid.Item>
-
-					<Grid.Item xl={ 12 }>
-						<Tabs>
-							<Tabs.Item selected={ tab === 'All Downloads' } onClick={ handleTabChange }>All Downloads</Tabs.Item>
-							<Tabs.Item selected={ tab === 'Paused' } onClick={ handleTabChange }>Paused</Tabs.Item>
-							<Tabs.Item selected={ tab === 'Cancelled' } onClick={ handleTabChange }>Cancelled</Tabs.Item>
-						</Tabs>
-					</Grid.Item>
-
-					{/* <Grid.Item sm={ 8 } style={  }>
-						<Grid.Item sm={ 2 }>
-							<Box >File Name</Box>
-						</Grid.Item>
-						<Grid.Item sm={ 1 }>
-							<Box >Server Title</Box>
-						</Grid.Item>
-						<Grid.Item sm={ 1 }>
-							<Box >File Size</Box>
-						</Grid.Item>
-						<Grid.Item sm={ 1 }>
-							<Box >Speed</Box>
-						</Grid.Item>
-						<Grid.Item sm={ 1 }>
-							<Box >Progress</Box>
-						</Grid.Item>
-						<Grid.Item sm={ 1 }>
-							<Box >Date</Box>
-						</Grid.Item>
-						<Grid.Item sm={ 1 }>
-							<Box >Remove</Box>
-						</Grid.Item>
 
 					</Grid.Item>
 
-					<Grid.Item sm={ 8 }>
-							<Divider />
-					</Grid.Item> */}
+					<Grid.Item xl={ 1 } sm={ 1 } className='tooltip' >
+						<Button ghost onClick={ clearAll }>
+							<Icon name='trash' size='x32' />
+							<Box>Delete All</Box>
+						</Button>
+					</Grid.Item>
+				</Grid.Item>
 
-					<Grid.Item xl={ 12 } style={ { display: 'flex', flexDirection: 'column', alignItems: 'center' } }>
-						{/* Download Item List */ }
-						{ filteredDownloads.map((downloadItem) => {
-							// Condition for Data Headings
-							if (!timeHeading) {
-								timeHeading = new Date(downloadItem.itemId).toDateString();
-							} else if (timeHeading === new Date(downloadItem.itemId).toDateString()) {
-								return <DownloadItem { ...downloadItem } updateDownloads={ updateDownloads } key={ downloadItem.itemId } layout={ layout } handleFileOpen={ handleFileOpen } clear={ clear } />;
-							}
+				{/* <Grid.Item sm={ 8 } style={  }>
+					<Grid.Item sm={ 2 }>
+						<Box >File Name</Box>
+					</Grid.Item>
+					<Grid.Item sm={ 1 }>
+						<Box >Server Title</Box>
+					</Grid.Item>
+					<Grid.Item sm={ 1 }>
+						<Box >File Size</Box>
+					</Grid.Item>
+					<Grid.Item sm={ 1 }>
+						<Box >Speed</Box>
+					</Grid.Item>
+					<Grid.Item sm={ 1 }>
+						<Box >Progress</Box>
+					</Grid.Item>
+					<Grid.Item sm={ 1 }>
+						<Box >Date</Box>
+					</Grid.Item>
+					<Grid.Item sm={ 1 }>
+						<Box >Remove</Box>
+					</Grid.Item>
+
+				</Grid.Item>
+
+				<Grid.Item sm={ 8 }>
+						<Divider />
+				</Grid.Item> */}
+
+				<Grid.Item xl={ 12 } style={ { display: 'flex', flexDirection: 'column', alignItems: 'center' } }>
+					{/* Download Item List */ }
+					{ filteredDownloads.map((downloadItem) => {
+						// Condition for Data Headings
+						if (!timeHeading) {
 							timeHeading = new Date(downloadItem.itemId).toDateString();
-							return (
-								<>
-									<Box fontSize='x16' lineHeight='2' color='info' alignSelf='start' paddingInlineStart='x20'>{ timeHeading }</Box>
-									<DownloadItem { ...downloadItem } updateDownloads={ updateDownloads } key={ downloadItem.itemId } layout={ layout } handleFileOpen={ handleFileOpen } clear={ clear } />
-								</>
-							);
-						}) }
-					</Grid.Item>
+						} else if (timeHeading === new Date(downloadItem.itemId).toDateString()) {
+							return <DownloadItem { ...downloadItem } updateDownloads={ updateDownloads } key={ downloadItem.itemId } layout={ layout } handleFileOpen={ handleFileOpen } clear={ clear } />;
+						}
+						timeHeading = new Date(downloadItem.itemId).toDateString();
+						return (
+							<>
+								<Box fontSize='x16' color='info' alignSelf='start'>{ timeHeading }</Box>
+								<DownloadItem mb='x16' { ...downloadItem } updateDownloads={ updateDownloads } key={ downloadItem.itemId } layout={ layout } handleFileOpen={ handleFileOpen } clear={ clear } />
+							</>
+						);
+					}) }
+				</Grid.Item>
 
-				</Grid>
-			</Box>
-		</Content>
+			</Grid>
+		</Box>
 	</Wrapper>;
 }

29b43a9d0bef09322b8e5bd877b120a2465015b9
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-08-07 20:06:21 -0300

    Add request-response logic to Redux actions

---
+++ src/rootWindow/channels.ts
@@ -0,0 +1,61 @@
+import { AnyAction } from 'redux';
+import { channel, stdChannel } from 'redux-saga';
+import { takeEvery, put, Effect } from 'redux-saga/effects';
+
+export type RequestAction<P> = AnyAction & {
+	payload: P;
+	meta: {
+		request: true;
+		id: string;
+	}
+};
+
+export const isRequest = (action: AnyAction): action is RequestAction<unknown> => action?.meta?.request;
+
+export type ResponseAction<P> = AnyAction & {
+	payload: P;
+	meta: {
+		response: true;
+		id: string;
+	}
+};
+
+export const isResponse = (action: AnyAction): action is RequestAction<unknown> => action?.meta?.response;
+
+export const requestsChannel = channel<RequestAction<unknown>>();
+export const responsesChannel = stdChannel<ResponseAction<unknown>>();
+
+export function *takeRequests(): Generator<Effect> {
+	yield takeEvery(requestsChannel, function *(action: AnyAction) {
+		yield put(action);
+	});
+
+	yield takeEvery(isResponse, function *(action) {
+		yield put(responsesChannel, action);
+	});
+}
+
+export const request = <P, RP>(actionType: string, payload: P): Promise<RP> =>
+	new Promise<RP>((resolve, reject) => {
+		const id = Math.random().toString(36).slice(2);
+
+		const requestAction: RequestAction<P> = {
+			type: actionType,
+			payload,
+			meta: {
+				request: true,
+				id,
+			},
+		};
+
+		requestsChannel.put(requestAction);
+
+		responsesChannel.take((action: ResponseAction<RP>) => {
+			if (action.error) {
+				reject(action.payload);
+				return;
+			}
+
+			resolve(action.payload);
+		}, (action) => action.meta.id === id);
+	});
--- src/rootWindow/rootSaga.ts
+++ src/rootWindow/rootSaga.ts
@@ -5,16 +5,17 @@ import { call, Effect } from 'redux-saga/effects';
 
 import { App } from '../components/App';
 import { whenReady } from '../whenReady';
+import { takeRequests } from './channels';
 import { setupErrorHandling } from './errors';
 import { setupI18next } from './i18n';
 
 
 export function *rootSaga(reduxStore: Store): Generator<Effect> {
+	yield *takeRequests();
+
 	yield call(async () => {
 		await whenReady();
-
 		setupErrorHandling(reduxStore);
-
 		await setupI18next();
 
 		render(createElement(App, { reduxStore }), document.getElementById('root'));

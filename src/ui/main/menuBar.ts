d4e5dbe99c803c23b1b3c64c288de8b0e7ee9c27
tarun8718 <tarunkumar8718@gmail.com>
2021-02-10 18:09:35 +0530

    Adds missing Accelerators
    Signed-off-by: tarun8718 <tarunkumar8718@gmail.com>

--- src/ui/main/menuBar.ts
+++ src/ui/main/menuBar.ts
@@ -251,6 +251,7 @@ const createViewMenu = createSelector(
         label: t('menus.showTrayIcon'),
         type: 'checkbox',
         checked: isTrayIconEnabled,
+        accelerator: process.platform === 'darwin' ? 'Shift+Command+T' : 'Ctrl+Shift+T',
         click: ({ checked }) => {
           dispatch({
             type: MENU_BAR_TOGGLE_IS_TRAY_ICON_ENABLED_CLICKED,
@@ -282,6 +283,7 @@ const createViewMenu = createSelector(
           label: t('menus.showMenuBar'),
           type: 'checkbox',
           checked: isMenuBarEnabled,
+          accelerator: process.platform === 'darwin' ? 'Shift+Command+M' : 'Ctrl+Shift+M',
           click: async ({ checked }) => {
             const browserWindow = await getRootWindow();
 
@@ -301,6 +303,7 @@ const createViewMenu = createSelector(
         label: t('menus.showServerList'),
         type: 'checkbox',
         checked: isSideBarEnabled,
+        accelerator: process.platform === 'darwin' ? 'Shift+Command+S' : 'Ctrl+Shift+S',
         click: async ({ checked }) => {
           const browserWindow = await getRootWindow();
 

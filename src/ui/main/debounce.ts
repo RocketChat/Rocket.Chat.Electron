e518713b1d114cc23577ffcb928ff83516be7629
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-09-06 15:02:04 -0300

    ReturnType<typeof setTimeout>

--- src/ui/main/debounce.ts
+++ src/ui/main/debounce.ts
@@ -2,9 +2,9 @@ export function debounce<T extends (...params: any[]) => unknown>(
   cb: T,
   wait = 20
 ): T {
-  let h: NodeJS.Timeout;
+  let h: ReturnType<typeof setTimeout> | undefined;
   const callable = (...args: any) => {
-    clearTimeout(h);
+    h && clearTimeout(h);
     h = setTimeout(() => cb(...args), wait);
   };
   return <T>(<any>callable);

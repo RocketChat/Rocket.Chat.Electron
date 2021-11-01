a8e731dffce1ac4bc0ccd9cb8a3a401b8ea8114e
Jean Brito <jeanfbrito@gmail.com>
2021-11-01 15:29:15 -0300

    add back desktop version to userAgent string

--- src/ui/main/serverView/index.ts
+++ src/ui/main/serverView/index.ts
@@ -232,10 +232,9 @@ export const attachGuestWebContentsEvents = async (): Promise<void> => {
 
           newWindow.loadURL(url, {
             userAgent: isGoogleSignIn
-              ? app.userAgentFallback.replace(
-                  `Electron/${process.versions.electron} `,
-                  ''
-                )
+              ? app.userAgentFallback
+                .replace(`Electron/${process.versions.electron} `, '')
+                .replace(`${app.name}/${app.getVersion()} `, '')
               : app.userAgentFallback,
             httpReferrer: referrer,
             ...(postBody && {

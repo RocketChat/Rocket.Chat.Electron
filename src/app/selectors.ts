9b660442f58b1fd5f957831e3eff9496b5af355a
Jean Brito <jeanfbrito@gmail.com>
2021-11-08 20:33:38 -0300

    add isInternalVideoChatWindowEnabled setting

--- src/app/selectors.ts
+++ src/app/selectors.ts
@@ -27,6 +27,5 @@ export const selectPersistableValues = createStructuredSelector<
   externalProtocols: ({ externalProtocols }) => externalProtocols,
   isReportEnabled: ({ isReportEnabled }) => isReportEnabled,
   isFlashFrameEnabled: ({ isFlashFrameEnabled }) => isFlashFrameEnabled,
-  isInternalVideoChatWindowEnabled: ({ isInternalVideoChatWindowEnabled }) =>
-    isInternalVideoChatWindowEnabled,
+  isInternalVideoChatWindowEnabled: ({ isInternalVideoChatWindowEnabled }) => isInternalVideoChatWindowEnabled,
 });

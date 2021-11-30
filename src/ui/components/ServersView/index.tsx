407c562531b5af841b40006f25aa657db56979f4
Guilherme Gazzo <guilhermegazzo@gmail.com>
2021-11-30 16:16:59 -0300

    fix: Empty screen from a fresh start + servers.json (#2243)
    Co-authored-by: Jean Brito <jeanfbrito@gmail.com>

--- src/ui/components/ServersView/index.tsx
+++ src/ui/components/ServersView/index.tsx
@@ -1,27 +1,11 @@
 import React, { FC } from 'react';
-import { useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
 
-import { RootState } from '../../../store/rootReducer';
+import { useServers } from '../hooks/useServers';
 import { ReparentingContainer } from '../utils/ReparentingContainer';
 import { ServerPane } from './ServerPane';
 
 export const ServersView: FC = () => {
-  const servers = useSelector(
-    createSelector(
-      ({ currentView }: RootState) => currentView,
-      ({ servers }: RootState) => servers,
-      (currentView, servers) =>
-        servers.map((server) =>
-          Object.assign(server, {
-            selected:
-              typeof currentView === 'object'
-                ? server.url === currentView.url
-                : false,
-          })
-        )
-    )
-  );
+  const servers = useServers();
 
   return (
     <ReparentingContainer>

c3732c5a6791a7bb311a98101674f260321f2a70
Jean Brito <jeanfbrito@gmail.com>
2022-03-16 14:55:10 -0300

    chore: Add open Devtools on all windows to menu

--- src/ui/main/menuBar.ts
+++ src/ui/main/menuBar.ts
@@ -231,11 +231,23 @@ const createViewMenu = createSelector(
         accelerator:
           process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
         click: () => {
-          // const guestWebContents =
-          //   typeof currentView === 'object'
-          //     ? getWebContentsByServerUrl(currentView.url)
-          //     : null;
-          // guestWebContents?.toggleDevTools();
+          const guestWebContents =
+            typeof currentView === 'object'
+              ? getWebContentsByServerUrl(currentView.url)
+              : null;
+          guestWebContents?.toggleDevTools();
+          // const windows = BrowserWindow.getAllWindows();
+          // windows.forEach((window) => {
+          //   window.webContents.toggleDevTools();
+          // });
+        },
+      }, {
+        id: 'openDevToolsOnAllWindows',
+        label: t('menus.openDevToolsOnAllWindows'),
+        enabled: typeof currentView === 'object' && !!currentView.url,
+        accelerator:
+          process.platform === 'darwin' ? 'Command+Alt+G' : 'Ctrl+Shift+G',
+        click: () => {
           const windows = BrowserWindow.getAllWindows();
           windows.forEach((window) => {
             window.webContents.toggleDevTools();

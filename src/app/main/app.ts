0fea358cb67186b1c10fcd92b5c24bc8cc4e64c2
Troplo <troplo@troplo.com>
2022-04-23 22:38:42 +1000

    Update app.ts

--- src/app/main/app.ts
+++ src/app/main/app.ts
@@ -16,6 +16,7 @@ export const performElectronStartup = (): void => {
   app.setAppUserModelId('chat.rocket');
 
   app.commandLine.appendSwitch('--autoplay-policy', 'no-user-gesture-required');
+  app.commandLine.appendSwitch('disable-features', 'HardwareMediaKeyHandling,MediaSessionService');
 
   const args = process.argv.slice(app.isPackaged ? 1 : 2);
 

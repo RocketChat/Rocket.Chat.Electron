c278b655a5516461538c11264402270acb356019
Jean Brito <jeanfbrito@gmail.com>
2022-08-08 10:17:29 -0300

    fix lint

--- src/ui/main/menuBar.ts
+++ src/ui/main/menuBar.ts
@@ -304,7 +304,8 @@ const createViewMenu = createSelector(
         label: t('menus.showTrayIcon'),
         type: 'checkbox',
         checked: isTrayIconEnabled,
-        accelerator: process.platform === 'darwin' ? 'Shift+Command+T' : 'Ctrl+Shift+T',
+        accelerator:
+          process.platform === 'darwin' ? 'Shift+Command+T' : 'Ctrl+Shift+T',
         click: ({ checked }) => {
           setTimeout(() => {
             dispatch({
@@ -338,7 +339,8 @@ const createViewMenu = createSelector(
           label: t('menus.showMenuBar'),
           type: 'checkbox',
           checked: isMenuBarEnabled,
-          accelerator: process.platform === 'darwin' ? 'Shift+Command+M' : 'Ctrl+Shift+M',
+          accelerator:
+            process.platform === 'darwin' ? 'Shift+Command+M' : 'Ctrl+Shift+M',
           click: async ({ checked }) => {
             const browserWindow = await getRootWindow();
 
@@ -358,7 +360,8 @@ const createViewMenu = createSelector(
         label: t('menus.showServerList'),
         type: 'checkbox',
         checked: isSideBarEnabled,
-        accelerator: process.platform === 'darwin' ? 'Shift+Command+S' : 'Ctrl+Shift+S',
+        accelerator:
+          process.platform === 'darwin' ? 'Shift+Command+S' : 'Ctrl+Shift+S',
         click: async ({ checked }) => {
           const browserWindow = await getRootWindow();
 

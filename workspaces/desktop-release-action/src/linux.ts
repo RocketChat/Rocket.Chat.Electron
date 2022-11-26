16aabba2ddcb91937dc498bd5255cdab3c6abf24
Jean Brito <jeanfbrito@gmail.com>
2022-11-25 21:34:07 -0300

    try another thing

--- workspaces/desktop-release-action/src/linux.ts
+++ workspaces/desktop-release-action/src/linux.ts
@@ -8,6 +8,9 @@ export const setupSnapcraft = (): Promise<void> =>
   core.group('Setup Snapcraft', async () => {
     await run(`sudo snap install snapcraft --classic --channel stable`);
     await run(`sudo apt install gnome-keyring`);
+    await run(`dbus-run-session -- bash --noprofile --norc`);
+    await run(`rm -rf ~/.local/share/keyrings`);
+    await run(`echo -n 'db' | gnome-keyring-daemon --unlock`);
     await run(`echo /snap/bin >> ${process.env.GITHUB_PATH}`);
     await run('sudo chown root:root /');
 

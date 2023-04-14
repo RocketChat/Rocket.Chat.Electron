b8e4e1f769702d5f0357945c754fff5be6f18bb3
Jean Brito <jeanfbrito@gmail.com>
2023-04-14 14:07:02 -0300

    increase bundleVersion for App Store

--- src/app/main/dev.ts
+++ src/app/main/dev.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { app, WebContents } from 'electron';
 
 export const setUserDataDirectory = (): void => {
-  if (process.env.NODE_ENV !== 'development') {
+  if (process.env.NODE_ENV !== 'development' || process.mas) {
     return;
   }
 
@@ -13,6 +13,7 @@ export const setUserDataDirectory = (): void => {
     fs.mkdirSync(folder, { recursive: true });
   }
 
+  console.log(`Setting user data directory to ${folder}`);
   app.setPath('userData', folder);
 };
 

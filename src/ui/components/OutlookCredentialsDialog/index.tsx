17a44d05304ea763a6d8cd084a01f2c10c5998ee
dougfabris <devfabris@gmail.com>
2023-05-09 19:00:00 -0300

    chore: RHF

--- src/ui/components/OutlookCredentialsDialog/index.tsx
+++ src/ui/components/OutlookCredentialsDialog/index.tsx
@@ -9,8 +9,10 @@ import {
   Margins,
   PasswordInput,
   TextInput,
+  ButtonGroup,
 } from '@rocket.chat/fuselage';
-import React, { ChangeEvent, FC, useEffect, useRef, useState } from 'react';
+import React, { FC, useEffect, useRef, useState, ReactElement } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useSelector, useDispatch } from 'react-redux';
 import { Dispatch } from 'redux';
@@ -27,37 +29,23 @@ import { isRequest } from '../../../store/fsa';
 import { RootState } from '../../../store/rootReducer';
 import { Dialog } from '../Dialog';
 
+export type AuthPayload = {
+  login: string;
+  password: string;
+  rememberCredentials?: boolean;
+};
+
 export const OutlookCredentialsDialog: FC = () => {
+  const { t } = useTranslation();
   const openDialog = useSelector(({ openDialog }: RootState) => openDialog);
   const isVisible = openDialog === 'outlook-credentials';
   const dispatch = useDispatch<Dispatch<RootAction>>();
-  const [server, setServer] = useState<Server | undefined>();
-  const [userId, setUserId] = useState<string>('');
-  const [isEncryptionAvailable, setIsEncryptionAvailable] = useState(false);
-  const [saveCredentials, setSaveCredentials] = useState(false);
 
   const requestIdRef = useRef<unknown>();
 
-  const [outlookLogin, setOutlookLogin] = useState<InputState>({
-    value: '',
-    required: false,
-  });
-  const [outlookPassword, setOutlookPassword] = useState<InputState>({
-    value: '',
-    required: false,
-  });
-
-  function handleInputChange(
-    setState: React.Dispatch<React.SetStateAction<InputState>>
-  ) {
-    return function (event: React.ChangeEvent<HTMLInputElement>) {
-      setState((prevState) => ({
-        ...prevState,
-        value: event.target.value,
-        required: event.target.value.trim() === '',
-      }));
-    };
-  }
+  const [server, setServer] = useState<Server | undefined>();
+  const [userId, setUserId] = useState<string>('');
+  const [isEncryptionAvailable, setIsEncryptionAvailable] = useState(false);
 
   useEffect(
     () =>
@@ -84,39 +72,12 @@ export const OutlookCredentialsDialog: FC = () => {
     });
   };
 
-  interface InputState {
-    value: string;
-    required: boolean;
-  }
-
-  const handleSaveCredentialsCheckboxChange = (
-    event: ChangeEvent<HTMLInputElement>
-  ): void => {
-    setSaveCredentials(event.target.checked);
-  };
-
-  function handleInputBlur(
-    setState: React.Dispatch<React.SetStateAction<InputState>>
-  ) {
-    return function () {
-      console.log('handleInputBlur');
-      setState((prevState) => ({
-        ...prevState,
-        required: prevState.value.trim() === '',
-      }));
-    };
-  }
-
-  const handleSubmit = async (): Promise<void> => {
-    if (
-      !server ||
-      !server.outlookCredentials ||
-      outlookLogin.value === '' ||
-      outlookPassword.value === ''
-    ) {
-      console.log('empty fields');
-      handleInputBlur(setOutlookLogin)();
-      handleInputBlur(setOutlookPassword)();
+  const handleAuth = async ({
+    login,
+    password,
+    rememberCredentials,
+  }: AuthPayload): Promise<void> => {
+    if (!server && !server?.outlookCredentials) {
       return;
     }
 
@@ -125,86 +86,97 @@ export const OutlookCredentialsDialog: FC = () => {
       payload: {
         url: server.url,
         outlookCredentials: {
-          login: outlookLogin.value,
-          password: outlookPassword.value,
+          login,
+          password,
           userId,
           serverUrl: server.outlookCredentials.serverUrl,
         },
-        saveCredentials,
+        saveCredentials: rememberCredentials,
       },
       meta: {
         response: true,
         id: requestIdRef.current,
       },
     });
-
-    setOutlookLogin({ value: '', required: false });
-    setOutlookPassword({ value: '', required: false });
   };
 
-  const { t } = useTranslation();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<AuthPayload>({
+    mode: 'onChange',
+    defaultValues: { rememberCredentials: false },
+  });
+
+  const { rememberCredentials } = watch();
 
   return (
     <Dialog isVisible={isVisible} onClose={handleCancel}>
-      <Box fontScale='h3'>Please enter your Outlook credentials</Box>
-      <FieldGroup>
-        <Field>
-          <Label>{t('Login')}</Label>
-          <Field.Row>
-            <TextInput
-              onChange={handleInputChange(setOutlookLogin)}
-              onBlur={handleInputBlur(setOutlookLogin)}
-              required={outlookLogin.required}
-              value={outlookLogin.value}
-            />
-          </Field.Row>
-          {outlookLogin.required && (
-            <Field.Error>{t('Field_required')}</Field.Error>
-          )}
-        </Field>
-        <Field>
-          <Label>{t('Password')}</Label>
-          <Field.Row>
-            <PasswordInput
-              onChange={handleInputChange(setOutlookPassword)}
-              onBlur={handleInputBlur(setOutlookPassword)}
-              value={outlookPassword.value}
-              required={outlookPassword.required}
-            />
-          </Field.Row>
-          {outlookPassword.required && (
-            <Field.Error>{t('Field_required')}</Field.Error>
-          )}
-        </Field>
-        {!isEncryptionAvailable && saveCredentials && (
-          <Callout title='Encryption unavailable' type='warning'>
-            Your operational system don't support encryption. <br />
-            Your credentials will be stored in plain text.
-          </Callout>
-        )}
-        <Field>
-          <Field.Row>
-            <CheckBox
-              id='check-box'
-              default={false}
-              onChange={handleSaveCredentialsCheckboxChange}
-            />
-            <Field.Label htmlFor='check-box'>
-              Remember my credentials
-            </Field.Label>
-          </Field.Row>
-        </Field>
-        <Box display='flex' alignItems='end' justifyContent='space-between'>
-          <Margins block='x8'>
-            <Button danger onClick={handleCancel}>
-              {t('Cancel')}
-            </Button>
-            <Button primary onClick={handleSubmit}>
-              {t('Submit')}
-            </Button>
-          </Margins>
+      <Box minWidth='x336'>
+        <Box fontScale='h3' mbe='x16'>
+          Outlook Calendar app
         </Box>
-      </FieldGroup>
+        <FieldGroup>
+          <Field>
+            <Label>{t('Login')}</Label>
+            <Field.Row>
+              <TextInput {...register('login', { required: true })} />
+            </Field.Row>
+            {errors.login && <Field.Error>{t('Field_required')}</Field.Error>}
+          </Field>
+          <Field>
+            <Label>{t('Password')}</Label>
+            <Field.Row>
+              <PasswordInput {...register('password', { required: true })} />
+            </Field.Row>
+            {errors.password && (
+              <Field.Error>{t('Field_required')}</Field.Error>
+            )}
+          </Field>
+          {!isEncryptionAvailable && rememberCredentials && (
+            <Callout title='Encryption unavailable' type='warning'>
+              Your operational system don't support encryption. <br />
+              Your credentials will be stored in plain text.
+            </Callout>
+          )}
+          <Field>
+            <Field.Row>
+              <Controller
+                control={control}
+                name='rememberCredentials'
+                render={({ field: { onChange, value, ref } }): ReactElement => (
+                  <CheckBox
+                    ref={ref}
+                    onChange={onChange}
+                    checked={value}
+                    id='check-box'
+                  />
+                )}
+              />
+              <Field.Label htmlFor='check-box'>
+                {t('Remember_my_credentials')}
+              </Field.Label>
+            </Field.Row>
+          </Field>
+          <Box display='flex' alignItems='end' justifyContent='space-between'>
+            <Margins block='x8'>
+              <ButtonGroup>
+                <Button onClick={handleCancel}>{t('Cancel')}</Button>
+                <Button
+                  primary
+                  disabled={isSubmitting}
+                  onClick={handleSubmit(handleAuth)}
+                >
+                  {t('Submit')}
+                </Button>
+              </ButtonGroup>
+            </Margins>
+          </Box>
+        </FieldGroup>
+      </Box>
     </Dialog>
   );
 };

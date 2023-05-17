cf8ab5052299687f1e7bcbb36f671f3c2d4f5767
Júlia Jaeger Foresti <60678893+juliajforesti@users.noreply.github.com>
2023-05-17 19:10:33 +0100

    fix: Sidebar color (#2655)
    * fix: change selector to get the correct sidebar color. fix loop
    
    * apply border
    
    * make new sidebar color work with older versions
    
    ---------
    
    Co-authored-by: Jean Brito <jeanfbrito@gmail.com>

--- src/servers/preload/sidebar.ts
+++ src/servers/preload/sidebar.ts
@@ -6,6 +6,30 @@ import { getServerUrl, getAbsoluteUrl } from './urls';
 let timer: ReturnType<typeof setTimeout>;
 let prevBackground: string;
 let prevColor: string;
+let prevBorder: string;
+let serverVersion: string;
+
+function versionIsGreaterOrEqualsTo(
+  version1: string,
+  version2: string
+): boolean {
+  const v1 = version1.match(/\d+/g)?.map(Number) || [];
+  const v2 = version2.match(/\d+/g)?.map(Number) || [];
+
+  for (let i = 0; i < 3; i++) {
+    const n1 = v1[i] || 0;
+    const n2 = v2[i] || 0;
+
+    if (n1 > n2) {
+      return true;
+    }
+    if (n1 < n2) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 const pollSidebarStyle = (
   referenceElement: Element,
@@ -14,19 +38,31 @@ const pollSidebarStyle = (
   clearTimeout(timer);
 
   document.body.append(referenceElement);
-  const { background, color } = window.getComputedStyle(referenceElement);
+  const { background, color, border } =
+    window.getComputedStyle(referenceElement);
+
   referenceElement.remove();
 
-  if (prevBackground !== background || prevColor !== color) {
+  const newBgg = prevBackground !== background ? background : prevBackground;
+  const newColor = prevColor !== color ? color : prevColor;
+  const newBorder = prevBorder !== border ? border : prevBorder;
+
+  if (
+    prevBackground !== background ||
+    prevColor !== color ||
+    newBorder !== border
+  ) {
     emit({
-      background,
-      color,
+      background: newBgg,
+      color: newColor,
+      border: newBorder,
     });
     prevBackground = background;
     prevColor = color;
+    prevBorder = border;
   }
 
-  timer = setTimeout(() => pollSidebarStyle(referenceElement, emit), 1000);
+  timer = setTimeout(() => pollSidebarStyle(referenceElement, emit), 5000);
 };
 
 let element: HTMLElement;
@@ -34,13 +70,22 @@ let element: HTMLElement;
 const getElement = (): HTMLElement => {
   if (!element) {
     element = document.createElement('div');
-    element.classList.add('sidebar');
     element.style.backgroundColor = 'var(--sidebar-background)';
     element.style.color = 'var(--sidebar-item-text-color)';
     element.style.display = 'none';
+    if (versionIsGreaterOrEqualsTo(serverVersion, '6.3.0')) {
+      element.classList.add('rcx-sidebar--main');
+      element.style.border = '1px solid var(--sidebar-border-color)';
+    } else {
+      element.classList.add('sidebar');
+    }
   }
 
   return element;
+};
+
+export const setServerVersionToSidebar = (version: string): void => {
+  serverVersion = version;
 };
 
 export const setBackground = (imageUrl: string): void => {

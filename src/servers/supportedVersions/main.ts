50f7ff184a6e941b796f68c1bb86a1cce6c05f7f
Jean Brito <jeanfbrito@gmail.com>
2023-10-23 10:52:34 -0300

    handle errors on fetching servers

--- src/servers/supportedVersions/main.ts
+++ src/servers/supportedVersions/main.ts
@@ -74,7 +74,8 @@ const getCloudInfo = (
   )
     .then((response) => {
       if (!response.ok) {
-        throw new Error(`Couldn't load Cloud Info: ${response.status}`);
+        console.log(`Couldn't load Cloud Info: ${response.status}`);
+        return null;
       }
       return response.json();
     })
@@ -90,7 +91,8 @@ export const getServerInfo = (serverUrl: string): Promise<ServerInfo | null> =>
   fetch(`${serverUrl}/api/info`)
     .then((response) => {
       if (!response.ok) {
-        throw new Error(`Couldn't load Server Info: ${response.status}`);
+        console.log(`Couldn't load Server Info: ${response.status}`);
+        return null;
       }
       return response.json();
     })

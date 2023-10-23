03f711a816d90f590c4fad0edc3a1197fdc03620
Jean Brito <jeanfbrito@gmail.com>
2023-10-23 16:47:18 -0300

    fix build in windows

--- src/servers/supportedVersions/downloadFromCloud.ts
+++ src/servers/supportedVersions/downloadFromCloud.ts
@@ -1,11 +1,12 @@
 import fs from 'fs';
+import path from 'path';
 
 import type { AxiosResponse } from 'axios';
 import axios from 'axios';
 
 const apiUrl =
   'https://releases.rocket.chat/v2/server/supportedVersions?source=desktop';
-const outputFilePath = './app/supportedVersions.jwt';
+const outputFilePath = path.join('.', 'app', 'supportedVersions.jwt');
 
 interface SupportedVersionsResponse {
   signed?: string;
@@ -13,16 +14,20 @@ interface SupportedVersionsResponse {
 
 export async function downloadAndSaveSupportedVersionsFromCloud(): Promise<void> {
   console.log('Getting Supported Versions from Cloud...');
+
   try {
     const response: AxiosResponse<SupportedVersionsResponse> = await axios.get(
       apiUrl
     );
-
     if (response.status === 200) {
       const jsonData: SupportedVersionsResponse = response.data;
 
       if (jsonData && jsonData.signed) {
         const signedContent: string = jsonData.signed;
+        const directory = path.dirname(outputFilePath);
+        if (!fs.existsSync(directory)) {
+          fs.mkdirSync(directory, { recursive: true });
+        }
 
         fs.writeFileSync(outputFilePath, signedContent);
         console.log('Signed content saved successfully.');
@@ -35,7 +40,10 @@ export async function downloadAndSaveSupportedVersionsFromCloud(): Promise<void>
       console.error('Failed to retrieve data from Cloud.');
     }
   } catch (error: any) {
-    console.error('Error getting the Supported Versions from Cloud:', error);
+    console.error(
+      'Error getting the Supported Versions from Cloud:',
+      error.message
+    );
   }
 }
 

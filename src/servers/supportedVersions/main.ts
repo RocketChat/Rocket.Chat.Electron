1710f7d07f0d60a28faf800d19a005c8a84ff15f
Jean Brito <jeanfbrito@gmail.com>
2023-12-01 17:28:07 -0300

    fix instance_domain

--- src/servers/supportedVersions/main.ts
+++ src/servers/supportedVersions/main.ts
@@ -210,7 +210,7 @@ export const getExpirationMessageTranslated = (
   const params = {
     instance_version: serverVersion,
     instance_ws_name: serverName,
-    instanceDomain: serverUrl,
+    instance_domain: serverUrl,
     remaining_days: moment(expiration).diff(new Date(), 'days'),
     ...message?.params,
   };

0ac013ebb6ece047de7fd85bdf661de4686018b3
Prisha Gupta <prishapg101@gmail.com>
2024-01-02 21:25:56 +0530

    Fix: sidebar not resizing with webview using both shortcuts and menu bar
    fixes: (#2444)

--- src/ui/main/menuBar.ts
+++ src/ui/main/menuBar.ts
@@ -1,5 +1,5 @@
 import type { MenuItemConstructorOptions } from 'electron';
-import { Menu, app, shell, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu, shell } from 'electron';
 import i18next from 'i18next';
 import { createSelector, createStructuredSelector } from 'reselect';
 
@@ -405,7 +405,15 @@ const createViewMenu = createSelector(
         accelerator: 'CommandOrControl+0',
         click: async () => {
           const guestWebContents = await getCurrentViewWebcontents();
+          if (!guestWebContents) {
+            return;
+          }
+
           guestWebContents?.setZoomLevel(0);
+
+          const rootWindow = await getRootWindow();
+          rootWindow.focus();
+          rootWindow.webContents.setZoomLevel(0);
         },
       },
       {
@@ -417,12 +425,16 @@ const createViewMenu = createSelector(
           if (!guestWebContents) {
             return;
           }
-          const zoomLevel = guestWebContents?.getZoomLevel();
+
+          const zoomLevel = guestWebContents.getZoomLevel();
           if (zoomLevel >= 9) {
             return;
           }
-
           guestWebContents.setZoomLevel(zoomLevel + 1);
+
+          const rootWindow = await getRootWindow();
+          rootWindow.focus();
+          rootWindow.webContents.setZoomLevel(zoomLevel + 1);
         },
       },
       {
@@ -438,8 +450,11 @@ const createViewMenu = createSelector(
           if (zoomLevel <= -9) {
             return;
           }
-
           guestWebContents.setZoomLevel(zoomLevel - 1);
+
+          const rootWindow = await getRootWindow();
+          rootWindow.focus();
+          rootWindow.webContents.setZoomLevel(zoomLevel - 1);
         },
       },
     ],

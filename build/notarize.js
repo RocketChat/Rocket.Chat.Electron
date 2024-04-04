49d7b0c35d34f8e9b3b05fbc96bd4b41f019a50e
Jean Brito <jeanfbrito@gmail.com>
2024-04-04 16:24:36 -0300

    update notarize.js

--- build/notarize.js
+++ build/notarize.js
@@ -1,4 +1,4 @@
-const { notarize } = require('electron-notarize');
+import { notarize } from '@electron/notarize';
 
 exports.default = function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;

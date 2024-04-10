cbc1bc003cfb0fe95370f193eb8b2c228bd56ad1
Jean Brito <jeanfbrito@gmail.com>
2024-04-10 16:31:49 -0300

    add clearing cache state for modal

--- src/ui/components/ClearCacheDialog/index.tsx
+++ src/ui/components/ClearCacheDialog/index.tsx
@@ -1,4 +1,10 @@
-import { Box, Button, ButtonGroup, Margins } from '@rocket.chat/fuselage';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Margins,
+  Throbber,
+} from '@rocket.chat/fuselage';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
@@ -18,8 +24,17 @@ export const ClearCacheDialog = () => {
   const dispatch = useDispatch<Dispatch<RootAction>>();
   const [webContendId, setWebcontentId] = useState<number>();
   const [isVisible, setIsVisible] = useState(false);
+  const [clearingCache, setClearingCache] = useState(false);
 
   const { t } = useTranslation();
+
+  const clearingCacheState = (): void => {
+    setClearingCache(true);
+    setTimeout(() => {
+      setIsVisible(false);
+      setClearingCache(false);
+    }, 2000);
+  };
 
   const handleKeepButtonClick = (): void => {
     if (webContendId === undefined) return;
@@ -27,7 +42,7 @@ export const ClearCacheDialog = () => {
       type: CLEAR_CACHE_DIALOG_KEEP_LOGIN_DATA_CLICKED,
       payload: webContendId,
     });
-    setIsVisible(false);
+    clearingCacheState();
   };
 
   const handleDeleteButtonClick = (): void => {
@@ -36,24 +51,26 @@ export const ClearCacheDialog = () => {
       type: CLEAR_CACHE_DIALOG_DELETE_LOGIN_DATA_CLICKED,
       payload: webContendId,
     });
-    setIsVisible(false);
+    clearingCacheState();
   };
 
   const handleClose = (): void => {
     dispatch({
       type: CLEAR_CACHE_DIALOG_DISMISSED,
     });
-    setIsVisible(false);
   };
 
-  useEffect(
-    () =>
-      listen(CLEAR_CACHE_TRIGGERED, async (action) => {
-        setWebcontentId(action.payload);
-        setIsVisible(true);
-      }),
-    [dispatch]
-  );
+  useEffect(() => {
+    listen(CLEAR_CACHE_TRIGGERED, async (action) => {
+      setWebcontentId(action.payload);
+      setIsVisible(true);
+    });
+
+    listen(CLEAR_CACHE_DIALOG_DISMISSED, () => {
+      setIsVisible(false);
+      setClearingCache(false);
+    });
+  }, [dispatch]);
 
   return (
     <Dialog isVisible={isVisible} onClose={handleClose}>
@@ -72,17 +89,32 @@ export const ClearCacheDialog = () => {
           </Box>
         </Margins>
       </Box>
-      <ButtonGroup stretch vertical>
-        <Button type='button' primary onClick={handleKeepButtonClick}>
-          {t('dialog.clearCache.keepLoginData')}
-        </Button>
-        <Button type='button' danger onClick={handleDeleteButtonClick}>
-          {t('dialog.clearCache.deleteLoginData')}
-        </Button>
-        <Button type='button' onClick={handleClose}>
-          {t('dialog.clearCache.cancel')}
-        </Button>
-      </ButtonGroup>
+      {!clearingCache && (
+        <ButtonGroup stretch vertical>
+          <Button type='button' primary onClick={handleKeepButtonClick}>
+            {t('dialog.clearCache.keepLoginData')}
+          </Button>
+          <Button type='button' danger onClick={handleDeleteButtonClick}>
+            {t('dialog.clearCache.deleteLoginData')}
+          </Button>
+          <Button type='button' onClick={handleClose}>
+            {t('dialog.clearCache.cancel')}
+          </Button>
+        </ButtonGroup>
+      )}
+      {clearingCache && (
+        <Box
+          width='x300'
+          display='flex'
+          flexDirection='column'
+          alignItems='center'
+        >
+          <Margins block='x20'>
+            <Box fontScale='h3'>{t('dialog.clearCache.clearingWait')}</Box>
+            <Throbber size='x16' />
+          </Margins>
+        </Box>
+      )}
     </Dialog>
   );
 };

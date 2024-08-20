0adef14d0f318fab36aa60c98f96b6bb01261a74
Jean Brito <jeanfbrito@gmail.com>
2024-08-20 11:14:26 -0300

    c

--- src/.jest/setup.ts
+++ src/.jest/setup.ts
@@ -27,3 +27,13 @@ expect.extend({
     };
   },
 });
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  process.exit(1);
+});
+
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  process.exit(1);
+});

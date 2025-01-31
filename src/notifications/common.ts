56dff5425f5056245887121e73ceb93ae0139491
Tasso <tasso.evangelista@rocket.chat>
2025-01-31 01:53:37 -0300

    Upgrade TypeScript

--- src/notifications/common.ts
+++ src/notifications/common.ts
@@ -2,4 +2,8 @@ export type ExtendedNotificationOptions = NotificationOptions & {
   canReply?: boolean;
   title: string;
   subtitle?: string;
+  actions?: {
+    title: string;
+  }[];
+  renotify?: boolean;
 };

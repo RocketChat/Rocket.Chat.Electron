50ec61e15e550c4db655b8f63ea48f98e83d5e39
Jean Brito <jeanfbrito@gmail.com>
2025-08-27 18:03:53 -0300

    lint fix

--- build/winSignKms.js
+++ build/winSignKms.js
@@ -9,15 +9,18 @@ module.exports = async function signWithGoogleKms(config) {
 
   const input = config.path;
   const name = config.name || 'Rocket.Chat';
-  const site = config.site;
+  const { site } = config;
 
   const signtoolPath = process.env.SIGNTOOL_PATH || 'signtool';
   const kmsCspName = process.env.WIN_KMS_CSP || 'Google Cloud KMS Provider';
   const kmsKeyResource = process.env.WIN_KMS_KEY_RESOURCE; // projects/.../cryptoKeys/.../cryptoKeyVersions/N
   const certSha1 = process.env.WIN_KMS_CERT_SHA1; // thumbprint of issued code signing cert installed in Windows cert store
-  const timestampUrl = process.env.WIN_TIMESTAMP_URL || 'http://timestamp.digicert.com';
+  const timestampUrl =
+    process.env.WIN_TIMESTAMP_URL || 'http://timestamp.digicert.com';
   const certStore = process.env.WIN_KMS_CERT_STORE || 'MY'; // Personal store by default
-  const useLocalMachine = /^true$/i.test(process.env.WIN_KMS_USE_LOCAL_MACHINE || 'false');
+  const useLocalMachine = /^true$/i.test(
+    process.env.WIN_KMS_USE_LOCAL_MACHINE || 'false'
+  );
 
   if (!kmsKeyResource) {
     throw new Error('[winSignKms] WIN_KMS_KEY_RESOURCE is required');
@@ -28,14 +31,22 @@ module.exports = async function signWithGoogleKms(config) {
 
   const args = [
     'sign',
-    '/fd', 'SHA256',
-    '/tr', timestampUrl,
-    '/td', 'SHA256',
-    '/csp', kmsCspName,
-    '/kc', kmsKeyResource,
-    '/sha1', certSha1,
-    '/s', certStore,
-    '/d', name,
+    '/fd',
+    'SHA256',
+    '/tr',
+    timestampUrl,
+    '/td',
+    'SHA256',
+    '/csp',
+    kmsCspName,
+    '/kc',
+    kmsKeyResource,
+    '/sha1',
+    certSha1,
+    '/s',
+    certStore,
+    '/d',
+    name,
   ];
 
   if (useLocalMachine) {
@@ -51,7 +62,10 @@ module.exports = async function signWithGoogleKms(config) {
   console.log('[winSignKms] Running:', signtoolPath, args.join(' '));
 
   await new Promise((resolve, reject) => {
-    const child = spawn(signtoolPath, args, { stdio: 'inherit', windowsHide: true });
+    const child = spawn(signtoolPath, args, {
+      stdio: 'inherit',
+      windowsHide: true,
+    });
     child.on('exit', (code) => {
       if (code === 0) return resolve();
       reject(new Error(`[winSignKms] signtool exited with code ${code}`));

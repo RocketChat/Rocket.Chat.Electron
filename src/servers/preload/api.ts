9ecbe17b0c9451aff08705c6192a132a0e9ec5f8
Tasso <tasso.evangelista@rocket.chat>
2025-09-10 23:16:46 -0300

    refactor: Use `@rocket.chat/desktop-api` type

--- src/servers/preload/api.ts
+++ src/servers/preload/api.ts
@@ -1,3 +1,5 @@
+import type { IRocketChatDesktop } from '@rocket.chat/desktop-api';
+
 import {
   createNotification,
   destroyNotification,
@@ -9,15 +11,12 @@ import {
   clearOutlookCredentials,
   setUserToken,
 } from '../../outlookCalendar/preload';
-import type { OutlookEventsResponse } from '../../outlookCalendar/type';
 import { setUserPresenceDetection } from '../../userPresence/preload';
-import type { Server } from '../common';
 import { setBadge } from './badge';
 import { writeTextToClipboard } from './clipboard';
 import { openDocumentViewer } from './documentViewer';
 import { setFavicon } from './favicon';
 import { setGitCommitHash } from './gitCommitHash';
-import type { videoCallWindowOptions } from './internalVideoChatWindow';
 import {
   getInternalVideoChatWindowEnabled,
   openInternalVideoChatWindow,
@@ -40,47 +39,16 @@ type ServerInfo = {
 export let serverInfo: ServerInfo;
 let cb = (_serverInfo: ServerInfo): void => undefined;
 
-export type RocketChatDesktopAPI = {
-  onReady: (cb: (serverInfo: ServerInfo) => void) => void;
-  setServerInfo: (serverInfo: ServerInfo) => void;
-  setUrlResolver: (getAbsoluteUrl: (relativePath?: string) => string) => void;
-  setBadge: (badge: Server['badge']) => void;
-  setFavicon: (faviconUrl: string) => void;
-  setBackground: (imageUrl: string) => void;
-  setSidebarCustomTheme: (customTheme: string) => void;
-  setTitle: (title: string) => void;
-  setUserLoggedIn: (userLoggedIn: boolean) => void;
-  setUserPresenceDetection: (options: {
-    isAutoAwayEnabled: boolean;
-    idleThreshold: number | null;
-    setUserOnline: (online: boolean) => void;
-  }) => void;
-  setUserThemeAppearance: (themeAppearance: Server['themeAppearance']) => void;
-  createNotification: (
-    options: NotificationOptions & {
-      canReply?: boolean;
-      title: string;
-      onEvent: (eventDescriptor: { type: string; detail: unknown }) => void;
-    }
-  ) => Promise<unknown>;
-  destroyNotification: (id: unknown) => void;
-  getInternalVideoChatWindowEnabled: () => boolean;
-  openInternalVideoChatWindow: (
-    url: string,
-    options: videoCallWindowOptions
-  ) => void;
-  setGitCommitHash: (gitCommitHash: string) => void;
-  writeTextToClipboard: (text: string) => void;
-  getOutlookEvents: (date: Date) => Promise<OutlookEventsResponse>;
-  setOutlookExchangeUrl: (url: string, userId: string) => void;
-  hasOutlookCredentials: () => Promise<boolean>;
-  clearOutlookCredentials: () => void;
-  setUserToken: (token: string, userId: string) => void;
-  openDocumentViewer: (url: string, format: string, options: any) => void;
-  reloadServer: () => void;
-};
+declare global {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  interface Window {
+    RocketChatDesktop: IRocketChatDesktop & {
+      reloadServer: () => void; // TODO: remove this after updating @rocket.chat/desktop-api
+    };
+  }
+}
 
-export const RocketChatDesktop: RocketChatDesktopAPI = {
+export const RocketChatDesktop: Window['RocketChatDesktop'] = {
   onReady: (c) => {
     if (serverInfo) {
       c(serverInfo);

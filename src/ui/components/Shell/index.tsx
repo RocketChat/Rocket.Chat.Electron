cd75eae107d15df9e951e47ba4ec3ba4083fe6df
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2026-01-23 16:43:57 -0300

    chore(theme): transparency mode not removing background of server view (#3156)

--- src/ui/components/Shell/index.tsx
+++ src/ui/components/Shell/index.tsx
@@ -104,7 +104,6 @@ export const Shell = () => {
             position='relative'
             alignSelf='stretch'
             flexBasis='1 1 auto'
-            bg={process.platform === 'darwin' ? 'light' : undefined}
           >
             <ServersView />
             <AddServerView />
